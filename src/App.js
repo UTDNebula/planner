2404e11e5e3dd104c7b8f1b917d42498144217b4
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-16 10:57:00 -0700

    Set up app routing
    This uses react-router-dom to introduce three main paths:
    - /, the default landing page
    - /auth, the interim authentication page
    - /schedules, /schedules/:id, the schedule list page and schedule
      planning page.
    
    TODO: Display list of schedules and handle sign-in.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import LandingPage from './landing';
+import SchedulePlanner from './planner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/schedules/:id/:part?">
+          <SchedulePlanner></SchedulePlanner>
+        </Route>
+        <Route path="/schedules">
+          {/* TODO: Display list of schedules */}
+        </Route>
+        <Route path="/auth">
+          {/* TODO: Handle sign-in */}
+        </Route>
+        <Route path="/">
+          <LandingPage></LandingPage>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
   );
