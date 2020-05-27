e96e324bbe753c7a2bf932c8dc19b57ff6ccd5bf
Aliah <asndeguzman@gmail.com>
2020-05-27 18:09:37 -0500

    sidebar and semester boards completed, draggable + droppable but but code has bugs

---
+++ src/initial-data.js
@@ -0,0 +1,35 @@
+const initialData = {
+    tasks: {
+        'task-1': {id: 'task-1', content: 'Hongjoong'},
+        'task-2': {id : 'task-2', content: 'Seonghwa'},
+        'task-3': {id: 'task-3', content: 'Yeosang'},
+        'task-4': {id: 'task-4', content: 'Yunho'},
+        'task-5': {id: 'task-5', content: 'Jongho'},
+        'task-6': {id : 'task-6', content: 'Wooyoung'},
+        'task-7': {id: 'task-7', content: 'San'},
+        'task-8': {id: 'task-8', content: 'Mingi'},
+        
+
+    },
+    semesters: {
+        'semester-1': {
+            id: 'semester-1',
+            title: 'fall',
+            taskIds: ['task-1', 'task-2'],
+        },
+        'semester-2': {
+            id: 'semester-2',
+            title: 'spring',
+            taskIds: [],
+        }
+    },
+    courselist: {
+        id: 'courselist',
+        title: 'Courses',
+        taskIds:['task-3','task-4'
+            ,'task-5', 'task-6','task-7'],
+    },
+    columnOrder: ['semester-1', 'semester-2'],
+};
+
+export default initialData; 
