32d344800920f3103e21bb9366dd7edca60ffc99
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-29 10:17:25 -0700

    Create reducers for schedule planning

---
+++ src/planner/reducers.js
@@ -0,0 +1,52 @@
+import { ADD_COURSE, CREATE_SCHEDULE, DELETE_SCHEDULE, UPDATE_SCHEDULE } from './actions';
+
+/**
+ * The main data store for actions.
+ *
+ * @param {Array<import("../lib/types").Schedule>} 
+ * @param {ScheduleAction}
+ */
+export default function schedules(state = [], action) {
+  switch (action.type) {
+    case ADD_COURSE:
+
+      return state;
+    case CREATE_SCHEDULE:
+      const newSchedule = action.payload;
+      return [
+        ...state,
+        newSchedule,
+      ];
+    case DELETE_SCHEDULE:
+      const filtered = action.payload.id;
+      // TODO: Persist change to user data.
+      return state.filter(schedule => schedule.id !== filtered);
+    case UPDATE_SCHEDULE:
+      const scheduleId = action.payload.id;
+      return {
+      };
+    default:
+      return state;
+  }
+}
+
+function activeSchedule(state = {}, action) {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+}
+
+/**
+ * Return a list of all courses that belong to a given schedule.
+ *
+ * @param {string} schedule The ID of the schedule
+ */
+function getAllCourses(state, schedule) {
+
+}
+
+function getCourseById(courseId) {
+
+}
