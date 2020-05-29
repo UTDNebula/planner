166507df5a54cd5c3d2ded061a7b7f8a926b9152
Willie Chalmers III <willieechalmers@gmail.com>
2020-05-29 10:23:54 -0700

    Fix typo in app initialization

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
-  );
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
