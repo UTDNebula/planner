456ebb951f24f0cc07397f3e228126b589753729
Willie Chalmers III <willieechalmers@gmail.com>
2020-06-08 09:39:33 -0700

    Update tsconfig.json
    Add TS rules that are a good idea for React. Also prepare for new App
    component.
    
    Warning: there are going to be some broken commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import * as React from "react";
+import React, { ReactElement } from 'react';
+import {
+  RouteComponentProps,
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom';
+import styled from 'styled-components'; 
+import {DragDropContext}  from 'react-beautiful-dnd'; 
+import LandingPage from './landing';
 import initial from './initial-data';
+import { SchedulePlanner } from './planner';
+import { ScheduleListPage } from './schedules';
+import { connector } from './lib';
 import { Sidebar } from './sidebar';
 import { AllBoards } from './all-boards'; 
-import styled from 'styled-components'; 
-
-import {DragDropContext}  from 'react-beautiful-dnd'; 
+import './App.css';
 
 const Wrapper = styled.div`
     display: flex; 
@@ -251,3 +261,38 @@ export class App extends React.Component<any, componentState>{
         
     }
 }
+
+
+
+/**
+ * The root of the UI heirarchy.
+ */
+// class App extends React.Component<RouteComponentProps> {
+
+//   public componentDidMount() {
+//     // Subscribe to schedules
+//     //@ts-ignore
+//     this.props.loadCourses();
+//   }
+
+//   public render(): ReactElement {
+//     return (
+//       <Switch>
+//         <Route path="/schedules/:id/:part?">
+//           <SchedulePlanner {...this.props}></SchedulePlanner>
+//         </Route>
+//         <Route path="/schedules">
+//           <ScheduleListPage {...this.props}></ScheduleListPage>
+//         </Route>
+//         <Route path="/auth">
+//           {/* TODO: Handle sign-in */}
+//         </Route>
+//         <Route path="/">
+//           <LandingPage></LandingPage>
+//         </Route>
+//       </Switch>
+//     );
+//   }
+// }
+
+// export default withRouter(connector(App));
