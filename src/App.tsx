bd5f115f7b318536aeea8ea9bb2cc09646f1ac5d
Willie Chalmers III <willieechalmers@gmail.com>
2020-06-08 11:21:37 -0700

    Fix stash conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import './App.css';
 import {
   RouteComponentProps,
   Switch,
---
+++ src/schedules/index.ts
@@ -0,0 +1,16 @@
+// import { connect } from 'react-redux';
+import ScheduleListPage from './ScheduleListPage';
+
+// const mapStateToProps = (state) => ({
+//   schedules: state.schedules,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+
+// });
+
+// const AllSchedules = connect(mapStateToProps, mapDispatchToProps);
+
+export {
+  ScheduleListPage,
+};
