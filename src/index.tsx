56ef4e60678e5e44363c6b21f2ee42440a830abf
Willie Chalmers III <willieechalmers@gmail.com>
2020-06-08 11:48:25 -0700

    Apply stash changes

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { initFirebase } from './lib/firebase-init';
 import { store } from './lib';
+import { BrowserRouter as Router } from 'react-router-dom';
 import config from './firebase-config';
 
 initFirebase(config);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 
