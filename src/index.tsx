778d8afe8f916c61a2200fb7a01d6b6654b20e23
Willie Chalmers III <willieechalmers@gmail.com>
2020-06-08 18:38:44 -0700

    style: Fix import order

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { initFirebase } from './lib/firebase-init';
 import { store } from './lib';
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 import config from './firebase-config';
+import './index.css';
 
 initFirebase(config);
 
