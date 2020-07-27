e00eec424673aa749a21b33b75d9748b94d7cc9c
Willie Chalmers III <willieechalmers@gmail.com>
2020-07-27 10:18:39 -0700

    refactor: Convert CourseSearch to function component
    This also adds styles using Material-UI.

--- src/planner/sidebar/CourseSearch.tsx
+++ src/planner/sidebar/CourseSearch.tsx
@@ -1,38 +1,54 @@
 import React from 'react';
 import { Course } from '../../store/catalog/types';
 import CourseCard from '../../courses/CourseCard';
-import { List, TextField, Paper } from '@material-ui/core';
+import {
+  List,
+  InputBase,
+  Paper,
+  IconButton,
+  makeStyles,
+  Theme,
+  createStyles,
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 
 interface CourseSearchProps {
   courses: Course[];
 }
 
-interface CourseSearchState {
-  results: Course[];
-  query: string;
-}
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    searchBox: {
+      height: 56,
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 8,
+    },
+  });
+});
 
 /**
  * A searchbox and results list.
  */
-export default class CourseSearch extends React.Component<CourseSearchProps, CourseSearchState> {
-  constructor(props: CourseSearchProps) {
-    super(props);
-    this.state = {
-      results: [],
-      query: '',
-    };
-  }
+export default function CourseSearch(props: CourseSearchProps): JSX.Element {
+  const [results, setResults] = React.useState<Course[]>([]);
+  const [query, setQuery] = React.useState('');
+
+  const classes = useStyles();
 
   /**
    * Updates the currently stored search results in state.
    */
-  private search = () => {
-    const results = this.props.courses;
-    this.setState({
-      ...this.state,
-      results,
-    });
+  const search = () => {
+    const results = props.courses;
+    setResults(results);
   };
 
   /**
@@ -40,32 +56,28 @@ export default class CourseSearch extends React.Component<CourseSearchProps, Cou
    *
    * @param e A form event
    */
-  private handleQueryUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState(
-      {
-        ...this.state,
-        query: e.target.value,
-      },
-      this.search,
-    );
+  const handleQueryUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    search();
   };
-
-  public render(): React.ReactNode {
-    const results = this.state.results.map((course, index) => (
-      <CourseCard key={course.id} index={index} course={course} enabled={true} />
-    ));
-    return (
-      <div>
-        <Paper elevation={2}>
-          <TextField
-            id="searchQuery"
-            label="Search for a course"
-            value={this.state.query}
-            onChange={this.handleQueryUpdate}
-          />
-        </Paper>
-        <List>{results}</List>
-      </div>
-    );
-  }
+  const resultBlocks = results.map((course, index) => (
+    <CourseCard key={course.id} index={index} course={course} enabled={true} />
+  ));
+  return (
+    <div>
+      <Paper elevation={2} className={classes.searchBox}>
+        <InputBase
+          id="searchQuery"
+          placeholder="Search courses"
+          value={query}
+          onChange={handleQueryUpdate}
+          className={classes.input}
+        />
+        <IconButton type="submit" className={classes.iconButton} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <List>{resultBlocks}</List>
+    </div>
+  );
 }
