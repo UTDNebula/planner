69f9c96899ce788b40b21b0a22a668183173df77
Willie Chalmers III <willieechalmers@gmail.com>
2020-07-29 17:24:10 -0700

    chore: Add schedule edit toggle button to app bar

--- src/planner/toolbar/SchedulePlannerAppBar.tsx
+++ src/planner/toolbar/SchedulePlannerAppBar.tsx
@@ -7,6 +7,7 @@ import {
   createStyles,
   Button,
   IconButton,
+  Switch,
 } from '@material-ui/core';
 import AppsIcon from '@material-ui/icons/Apps';
 import { Link } from 'react-router-dom';
@@ -19,10 +20,16 @@ interface SchedulePlannerAppBarProps {
    * The title of the current screen.
    */
   title: string;
+
   /**
    * A callback notified when the save button is pressed.
    */
   onTriggerSave: () => void;
+
+  /**
+   * A callback notified when a schedule's editability changes.
+   */
+  onToggleEdit: (shouldEdit: boolean) => void;
 }
 
 /**
@@ -32,6 +39,9 @@ const useStyles = makeStyles((theme) => {
   return createStyles({
     title: {
       flexGrow: 1,
+    },
+    toggleLabel: {
+      marginRight: theme.spacing(1),
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -44,6 +54,14 @@ const useStyles = makeStyles((theme) => {
  */
 export default function SchedulePlannerAppBar(props: SchedulePlannerAppBarProps): JSX.Element {
   const classes = useStyles();
+  const [inEditMode, setInEditMode] = React.useState(false);
+
+  const handleEditToggle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const allowEditing = event.target.checked;
+    setInEditMode(allowEditing);
+    props.onToggleEdit(allowEditing);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -60,6 +78,15 @@ export default function SchedulePlannerAppBar(props: SchedulePlannerAppBarProps)
         <Typography variant="h6" color="inherit" className={classes.title}>
           {props.title}
         </Typography>
+        <Typography variant="subtitle1" color="inherit" className={classes.toggleLabel}>
+          {inEditMode ? 'Editing schedule' : 'Viewing schedule'}
+        </Typography>
+        <Switch
+          checked={inEditMode}
+          onChange={handleEditToggle}
+          name="editToggle"
+          inputProps={{ 'aria-label': 'Toggle editing mode' }}
+        />
         <Button onClick={props.onTriggerSave}>Save</Button>
       </Toolbar>
     </AppBar>
