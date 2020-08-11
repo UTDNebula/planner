7e33093ad1ee8ea6176dcee6a407f11499c51bcb
Willie Chalmers III <willie@williecubed.me>
2020-08-11 13:25:48 -0500

    refactor: Convert SemesterBlock to functional component

--- src/planner/SemesterBlock.tsx
+++ src/planner/SemesterBlock.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import CourseCard from '../courses/CourseCard';
 import { Droppable, DroppableProvided } from 'react-beautiful-dnd';
 import { ScheduleSemester } from '../lib/types';
-import Box from '@material-ui/core/Box';
-import List from '@material-ui/core/List';
-import { Typography } from '@material-ui/core';
+import { Box, List, Typography } from '@material-ui/core';
 import './SemesterBlock.css';
 
 interface SemesterBlockProps {
@@ -12,51 +10,38 @@ interface SemesterBlockProps {
   semester: ScheduleSemester;
 }
 
-interface SemesterBlockState {
-  courses: Array<any>; // TODO: Fix me
+function convertTermToText(term: string) {
+  return `Term ${term}`;
 }
 
 /**
  * A semester containing a list of CourseCards.
  */
-export default class SemesterBlock extends React.Component<SemesterBlockProps, SemesterBlockState> {
-  constructor(props: SemesterBlockProps) {
-    super(props);
-    this.state = {
-      courses: [],
-    };
-  }
-
-  private get termText() {
-    const termCode = this.props.semester.term;
-    return `Term ${termCode}`;
-  }
-
-  onCourseDrag() {}
-
-  render(): JSX.Element {
-    const displayedCourses = this.props.semester.courses.map((course, index) => {
-      return (
-        <CourseCard
-          key={`${course.subject} ${course.suffix}`}
-          index={index}
-          course={course}
-          enabled={this.props.enabled}
-        ></CourseCard>
-      );
-    });
+function SemesterBlock(props: SemesterBlockProps): JSX.Element {
+  const { enabled, semester } = props;
+  const displayedCourses = semester.courses.map((course, index) => {
     return (
-      <Box border={1} className="semester-block">
-        <Typography variant="h6">{this.termText}</Typography>
-        <Droppable droppableId={this.props.semester.term}>
-          {(provided: DroppableProvided, _) => (
-            <List ref={provided.innerRef} {...provided.droppableProps}>
-              {displayedCourses}
-              {provided.placeholder}
-            </List>
-          )}
-        </Droppable>
-      </Box>
+      <CourseCard
+        key={`${course.subject} ${course.suffix}`}
+        index={index}
+        course={course}
+        enabled={enabled}
+       />
     );
-  }
+  });
+  return (
+    <Box border={1} className="semester-block">
+      <Typography variant="h6">{convertTermToText(semester.term)}</Typography>
+      <Droppable droppableId={semester.term}>
+        {(provided: DroppableProvided, _) => (
+          <List ref={provided.innerRef} {...provided.droppableProps}>
+            {displayedCourses}
+            {provided.placeholder}
+          </List>
+        )}
+      </Droppable>
+    </Box>
+  );
 }
+
+export default SemesterBlock;
