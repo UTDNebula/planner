bb35ea1f7daa30bd95abc025bfcfd5d5948495bc
Willie Chalmers III <dev@williecubed.me>
2020-12-18 22:04:37 -0800

    chore: Add mock Schedule loading logic

--- src/features/planner/DegreePlannerChrome.tsx
+++ src/features/planner/DegreePlannerChrome.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 /**
  * Backing data type for a SemesterBlock.
@@ -14,11 +15,48 @@ interface Semester {
 interface DegreePlannerChromeProps {
 }
 
+interface AppParams {
+  planId: string;
+}
+
 /**
  * The root degree planner editor component.
  */
 export default function DegreePlannerChrome(props: DegreePlannerChromeProps) {
   const [semesters, setSemesters] = React.useState<Semester[]>([]);
+
+
+  function loadSchedule(scheduleId: string) {
+    const schedule = {
+      id: scheduleId,
+      title: 'Test schedule',
+      major: 'Computer Science',
+      semesters: [
+        {
+          title: '2021 Spring',
+          code: '2021s',
+        },
+        {
+          title: '2021 Fall',
+          code: '2021f',
+        },
+        {
+          title: '2022 Spring',
+          code: '2022s',
+        },
+      ],
+    };
+    return schedule;
+  }
+
+  const { planId } = useParams<AppParams>();
+
+  React.useEffect(() => {
+    // Load schedule into memory, prepare for modification
+    const loadedSchedule = loadSchedule(planId);
+    const loadedSemesters = loadedSchedule.semesters;
+    setSemesters(loadedSemesters);
+  }, [planId]);
 
   const semesterNavList = semesters.map(({ title }) => {
     return (
