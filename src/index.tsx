86fde932bc8aca6a2daa25f90b5521d89c5ba384
Willie Chalmers III <willie.chalmers@acmutd.co>
2020-12-19 23:30:16 -0800

    chore: Add Roboto font to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// TODO: Customize font using https://github.com/fontsource/fontsource/blob/master/packages/roboto/README.md
+import 'fontsource-roboto';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
