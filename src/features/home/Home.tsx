8531ed037c01631225f4ecbce3313a15eb5118fb
Willie Chalmers III <willie.chalmers@acmutd.co>
2020-12-20 23:05:52 -0800

    chore: Add degree plan snippet to home screen

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Container, createStyles, makeStyles, Theme, Toolbar } from '@material-ui/core';
 import HomePlanBlock from './HomePlanBlock';
 import HomeUserAnnouncementsBlock from './HomeUserAnnouncementsBlock';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
+    height: '100%',
     flexGrow: 1,
+    padding: theme.spacing(2),
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+  },
+  child: {
   },
 }));
 
@@ -18,11 +22,10 @@ export default function Home() {
   const classes = useStyles();
 
   return (
-    <div>
-      <div className={classes.root}>
+      <Container className={classes.root}>
+        <Toolbar /> {/* TODO: Fix toolbar for spacing */}
         <HomeUserAnnouncementsBlock />
         <HomePlanBlock />
-      </div>
-    </div>
+      </Container>
   );
 }
--- src/features/home/HomePlanBlock.tsx
+++ src/features/home/HomePlanBlock.tsx
@@ -1,6 +1,8 @@
 import { makeStyles, Theme, createStyles, Button, Typography } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { createSamplePlan } from '../../app/data';
+import SemesterBlock from '../planner/SemesterBlock';
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -12,25 +14,87 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  block: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+  },
+  semesterDisplay: {
+    display: 'flex',
+    marginTop: theme.spacing(2),
+    flexWrap: 'wrap',
+  },
+  laterSemesterList: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  semesterItem: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
 }));
 
+/**
+ * A component that displays the currently active degree plan.
+ */
 export default function HomePlanBlock() {
   const classes = useStyles();
+
+  const plan = createSamplePlan();
+
+  const startSemester = 0;
+
+  // TODO: Determine next semester using current semester
+  const currentSemester = plan.semesters[startSemester];
+
+  const firstBlock = {
+    semesterCode: currentSemester.code,
+    semesterTitle: currentSemester.title,
+    courses: currentSemester.courses,
+    onAddCourse: () => { },
+    onShowSemesterInfo: () => { },
+    onClearSemester: () => { },
+    onRemoveSemester: () => { },
+  };
+
+  const laterSemesters = plan.semesters.slice(startSemester + 1, startSemester + 4)
+    .map((semester) => {
+      return (
+        <div className={classes.semesterItem}>
+          <SemesterBlock displayOnly
+            semesterCode={semester.code}
+            semesterTitle={semester.title}
+            courses={semester.courses}
+            onAddCourse={() => { }}
+            onShowSemesterInfo={() => { }}
+            onClearSemester={() => { }}
+            onRemoveSemester={() => { }}
+          />
+        </div>
+      );
+    });
+
   return (
     <section className={classes.root}>
       <header className={classes.header}>
         <Typography variant="h5">Your current plan</Typography>
+        <Button variant="contained" color="primary" component={Link} to="/plans/new">Open plan</Button>
       </header>
-      <div>
-        <div>
+      <div className={classes.semesterDisplay}>
+        <div className={classes.block}>
           <Typography variant="subtitle1">This semester</Typography>
-          <div>
-            <Button color="primary" component={Link} to="/plans/new">Start planning</Button>
+          <div className={classes.semesterItem}>
+            <SemesterBlock displayOnly {...firstBlock}></SemesterBlock>
           </div>
         </div>
-        <div>
+        <div className={classes.block}>
           <Typography variant="subtitle1">What's next</Typography>
+          <div className={classes.laterSemesterList}>
+            {laterSemesters}
+          </div>
         </div>
+        {/* <div>
+          <Button color="primary" component={Link} to="/plans/new">Start planning</Button>
+        </div> */}
       </div>
     </section>
   );
