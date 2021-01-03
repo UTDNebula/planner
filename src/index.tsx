1f28798f1677f45bf9b6a118b097ba6ae9ef5a0e
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-03 00:02:32 -0600

    chore: Update styles
    This adds Tailwind CSS to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // TODO: Customize font using https://github.com/fontsource/fontsource/blob/master/packages/roboto/README.md
-import 'fontsource-roboto';
+import '@fontsource/roboto';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { CssBaseline } from '@material-ui/core';
 
 const rootElement = document.getElementById('root');
 
 const tree = (
   <React.StrictMode>
+    <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
