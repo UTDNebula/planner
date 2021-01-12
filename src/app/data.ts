bee8d8289293b4482790deaadb51122f326fb837
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-11 20:05:08 -0600

    chore: Update Course data type
    Include creditHours property.

--- src/app/data.ts
+++ src/app/data.ts
@@ -6,6 +6,7 @@ export interface Course {
   title: string;
   catalogCode: string;
   description: string;
+  creditHours: number;
 }
 
 /**
@@ -31,39 +32,57 @@ export interface StudentPlan {
  * A season when courses can be taken.
  */
 export enum SemesterCode {
-  'f' = 0,
-  's' = 1,
-  'u' = 2,
+  'f' = 'f',
+  's' = 's',
+  'u' = 'u',
 }
 
 /**
  * A grade received for a course, used for record-keeping.
  */
-export type Grade = 'A+' | 'A' | 'A-' | 'B+' | 'B' | 'B-' | 'C+' | 'C' | 'C-' | 'D+' | 'D' | 'D-' | 'F' | 'CR' | 'NC' | 'P' | 'NR' | 'I';
+export type Grade =
+  | 'A+'
+  | 'A'
+  | 'A-'
+  | 'B+'
+  | 'B'
+  | 'B-'
+  | 'C+'
+  | 'C'
+  | 'C-'
+  | 'D+'
+  | 'D'
+  | 'D-'
+  | 'F'
+  | 'CR'
+  | 'NC'
+  | 'P'
+  | 'NR'
+  | 'I';
 
 /**
  * A value of -1 indicates the course for which the letter grade was recieved
  * should not have its attmpted hours used in GPA calculations.
  */
 export const GPA_MAPPINGS: { [key in Grade]: number } = {
-  'A+': 4.00,
-  'A': 4.00,
+  'A+': 4.0,
+  A: 4.0,
   'A-': 3.666,
   'B+': 3.333,
-  'B': 3.000,
+  B: 3.0,
   'B-': 2.666,
   'C+': 2.333,
-  'C': 2.000,
+  C: 2.0,
   'C-': 1.333,
-  'D+': 1.000,
-  'D': 0.666,
+  'D+': 1.0,
+  D: 0.666,
   'D-': 0.333,
-  'F': 0,
-  'CR': -1,
-  'NC': -1,
-  'P': -1,
-  'NR': -1,
-  'I': -1,
+  F: 0,
+  CR: -1,
+  NC: -1,
+  P: -1,
+  NR: -1,
+  I: -1,
 };
 
 /**
@@ -77,10 +96,10 @@ export const SEMESTER_CODE_MAPPINGS = {
 
 /**
  * Generate a list of courses.
- * 
+ *
  * @param amount The count of courses to generate, 5 by default.
  */
-export function generateCourses(amount: number = 5): Course[] {
+export function generateCourses(amount = 5): Course[] {
   const courses = [];
   for (let i = 0; i < amount; ++i) {
     const id = Math.floor(Math.random() * (5000 - 1000) + 1000);
@@ -89,6 +108,7 @@ export function generateCourses(amount: number = 5): Course[] {
       title: 'A course with a code.',
       catalogCode: `CS ${id}`,
       description: 'Just another course. What can we say?',
+      creditHours: 3,
     });
   }
   return courses;
@@ -112,15 +132,14 @@ export function createSamplePlan(
   title = 'My degree plan',
   major = 'Computer Science',
 ): StudentPlan {
-
-  function generateSemesters(onlyLong: boolean = true): Semester[] {
+  function generateSemesters(onlyLong = true): Semester[] {
     const result = [];
     let semester = startSemester;
     let year = startYear;
     for (let i = 0; i < semesterCount; ++i) {
       const code = `${year}${semester}`;
       const newSemester = {
-        title: `${SEMESTER_CODE_MAPPINGS[semester]} ${year} `,
+        title: `${SEMESTER_CODE_MAPPINGS[semester]} ${year}`,
         code: code,
         courses: generateCourses(),
       };
@@ -128,7 +147,8 @@ export function createSamplePlan(
       if (semester === SemesterCode.f) {
         year = year + 1;
         semester = SemesterCode.s;
-      } else { // Semester code is either spring or summer
+      } else {
+        // Semester code is either spring or summer
         if (onlyLong || semester === SemesterCode.s) {
           semester = SemesterCode.f;
         } else {
