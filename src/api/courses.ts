09f057f4924ce4c5a753c475a698e1480b9b2cdf
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-12 22:41:54 -0600

    chore: Temporarily store 2020 course data

---
+++ src/api/courses.ts
@@ -0,0 +1,24 @@
+import { Course } from '../app/data';
+
+/**
+ * Load all supported courses from file.
+ *
+ * This maps the course objects in a course data file to a list of Course
+ * objects.
+ *
+ * @param year The catalog year from which to load course data
+ */
+export async function loadCourses(year = 2020): Promise<Course[]> {
+  const courseData: { [key: string]: any } = await import(`../../data/${year}-courses.json`);
+  return Object.entries(courseData).map((value) => {
+    const [catalogCode, courseData] = value;
+    const { id, name: title, hours: creditHours, description } = courseData;
+    return {
+      id: String(id),
+      title,
+      catalogCode,
+      description,
+      creditHours: Number(creditHours),
+    };
+  });
+}
