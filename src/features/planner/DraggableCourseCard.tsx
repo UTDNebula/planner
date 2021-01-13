3ab7d68f4d39e71eb98524ee461759e31fdee1c2
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-12 23:01:44 -0600

    fix: Update DraggableCourseCard API
    This uses the creditHours prop and the revised enabled prop from the
    base CourseCard component.

--- src/features/planner/DraggableCourseCard.tsx
+++ src/features/planner/DraggableCourseCard.tsx
@@ -6,32 +6,41 @@ import CourseCard, { CourseCardProps } from './CourseCard';
  * Component properties for a {@link DraggableCourseCard}.
  */
 export interface DraggableCourseCardProps extends CourseCardProps {
+  /**
+   * A unique identifier and key for this card.
+   */
   id: string;
+
+  /**
+   * This item's index in a list, used for drag and drop functionality.
+   */
   index: number;
+
+  /**
+   * If true, drag and drop functionality is allowed for this card.
+   */
+  enabled: boolean;
 }
 
 /**
  * A draggable card showing course details.
  */
-export default function DraggableCourseCard(props: DraggableCourseCardProps) {
-  const { id, code, title, description, index, showOptions } = props;
+export default function DraggableCourseCard(props: DraggableCourseCardProps): JSX.Element {
+  const { id, code, title, description, index, enabled, creditHours } = props;
   return (
-    <Draggable
-      key={id}
-      draggableId={id}
-      index={index}
-      isDragDisabled={false}>
+    <Draggable key={id} draggableId={id} index={index} isDragDisabled={!enabled}>
       {(provided) => (
         <CourseCard
           ref={provided.innerRef}
           code={code}
           title={title}
           description={description}
+          creditHours={creditHours}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          showOptions={showOptions}
+          enabled={enabled}
         />
       )}
     </Draggable>
   );
-};
+}
