52d264a82411d9bedb71d093453cdda5f747dbb7
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-13 01:36:07 -0600

    fix: HomePlanBlock semesters display

--- src/features/home/HomePlanBlock.tsx
+++ src/features/home/HomePlanBlock.tsx
@@ -1,37 +1,42 @@
 import { makeStyles, Theme, createStyles, Button, Typography } from '@material-ui/core';
 import React from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { Link } from 'react-router-dom';
 import { createSamplePlan } from '../../app/data';
 import SemesterBlock from '../planner/SemesterBlock';
+import SemesterBlockList, { ScrollDirection } from '../planner/SemesterBlockList';
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    flexGrow: 1,
-  },
-  header: {},
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  block: {
-    marginLeft: theme.spacing(2),
-    marginRight: theme.spacing(2),
-  },
-  semesterDisplay: {
-    display: 'flex',
-    marginTop: theme.spacing(2),
-    flexWrap: 'wrap',
-  },
-  laterSemesterList: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  semesterItem: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-  },
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    header: {},
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    block: {
+      // paddingTop: theme.spacing(2),
+      // marginRight: theme.spacing(2),
+      // margin: theme.spacing(2),
+    },
+    semesterDisplay: {
+      display: 'flex',
+      marginTop: theme.spacing(2),
+      flexWrap: 'wrap',
+    },
+    laterSemesterList: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    semesterItem: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+  }),
+);
 
 /**
  * A component that displays the currently active degree plan.
@@ -50,52 +55,41 @@ export default function HomePlanBlock() {
     semesterCode: currentSemester.code,
     semesterTitle: currentSemester.title,
     courses: currentSemester.courses,
-    onAddCourse: () => { },
-    onShowSemesterInfo: () => { },
-    onClearSemester: () => { },
-    onRemoveSemester: () => { },
   };
 
-  const laterSemesters = plan.semesters.slice(startSemester + 1, startSemester + 4)
-    .map((semester) => {
-      return (
-        <div className={classes.semesterItem}>
-          <SemesterBlock displayOnly
-            semesterCode={semester.code}
-            semesterTitle={semester.title}
-            courses={semester.courses}
-            onAddCourse={() => { }}
-            onShowSemesterInfo={() => { }}
-            onClearSemester={() => { }}
-            onRemoveSemester={() => { }}
-          />
-        </div>
-      );
-    });
+  const laterSemesters = plan.semesters.slice(startSemester + 1, startSemester + 4);
 
   return (
     <section className={classes.root}>
       <header className={classes.header}>
         <Typography variant="h5">Your current plan</Typography>
-        <Button variant="contained" color="primary" component={Link} to="/app/plans/new">Open plan</Button>
+        <Button variant="contained" color="primary" component={Link} to="/app/plans/new">
+          Open plan
+        </Button>
       </header>
-      <div className={classes.semesterDisplay}>
-        <div className={classes.block}>
-          <Typography variant="subtitle1">This semester</Typography>
-          <div className={classes.semesterItem}>
-            <SemesterBlock displayOnly {...firstBlock}></SemesterBlock>
+      <DragDropContext onDragEnd={() => console.log('Drag ended')}>
+        <div className={classes.semesterDisplay}>
+          <div className={classes.block}>
+            <div className="px-4 text-headline6">This semester</div>
+            <div className={classes.semesterItem}>
+              <SemesterBlock enabled={false} {...firstBlock} />
+            </div>
           </div>
-        </div>
-        <div className={classes.block}>
-          <Typography variant="subtitle1">What's next</Typography>
-          <div className={classes.laterSemesterList}>
-            {laterSemesters}
+          <div className={classes.block}>
+            <div className="px-4 text-headline6">Upcoming semesters</div>
+            {/* TODO: Remove need for DragDropContext */}
+            <SemesterBlockList
+              semesters={laterSemesters}
+              enabled={false}
+              direction={ScrollDirection.horizontally}
+              focusedSemester={''}
+            />
           </div>
-        </div>
-        {/* <div>
+          {/* <div>
           <Button color="primary" component={Link} to="/plans/new">Start planning</Button>
         </div> */}
-      </div>
+        </div>
+      </DragDropContext>
     </section>
   );
 }
