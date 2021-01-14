6a46140dc7434b6af8c997ae132b2214834ce9a9
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-13 23:54:23 -0600

    chore: Start redesigning home page UI

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import { Container, createStyles, makeStyles, Theme, Toolbar } from '@material-ui/core';
-import HomePlanBlock from './HomePlanBlock';
-import HomeUserAnnouncementsBlock from './HomeUserAnnouncementsBlock';
+import { Toolbar } from '@material-ui/core';
+import styles from './Home.module.css';
+import { NoticeBlock } from '../../components/home/announcements/NoticeBlock';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      height: '100%',
-      flexGrow: 1,
-      padding: theme.spacing(2),
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-    child: {},
-  }),
-);
-
-export default function Home() {
-  const classes = useStyles();
-
+/**
+ * The home screen for the app.
+ */
+export default function Home(): JSX.Element {
   return (
-    <Container className={classes.root}>
+    <>
       <Toolbar /> {/* TODO: Fix toolbar for spacing */}
-      <HomeUserAnnouncementsBlock />
-      <HomePlanBlock />
-    </Container>
+      <section className="container mx-auto md:grid min-h-full lg:grid-cols-12">
+        <div className="lg:col-span-4 bg-blue-200 rounded-md m-2 p-4">
+          <h1 className="text-headline5">Student Name</h1>
+          <div className="text-subtitle1">Junior studying Computer Science</div>
+          <div className="text-caption">A Computer Science Scholar</div>
+        </div>
+        <div className="lg:col-span-8 bg-gray-200 rounded-md m-2 p-4 border-2 border-gray-300">
+          <NoticeBlock />
+        </div>
+      </section>
+      <section className="container mx-auto md:flex m-4 border-gray-400 shadow-md">
+        <div className={styles.blockCardVariant}>
+          <div className="text-headline5">This semester</div>
+          {/* Courses */}
+        </div>
+        <div className={styles.blockCard}>
+          <div className="text-headline5">What&apos;s next</div>
+        </div>
+      </section>
+    </>
+    // <Container className={classes.root}>
+    //   <HomeUserAnnouncementsBlock />
+    //   <HomePlanBlock />
+    // </Container>
   );
 }
