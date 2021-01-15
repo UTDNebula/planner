c3dfc424bb49ef6591e5045181502e816c22eb5d
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-01-14 23:31:03 -0600

    chore: Add new student data types

---
+++ src/app/student.ts
@@ -0,0 +1,23 @@
+/**
+ * The registrar-determined year classification based on credit hours.
+ */
+export type YearClassification = 'fr' | 'so' | 'ju' | 'se' | 'gr';
+
+/**
+ * All the valid year classifications.
+ *
+ * @readonly
+ * @enum {string}
+ */
+export const CLASSIFICATIONS: { [key in YearClassification]: string } = {
+  fr: 'Freshman',
+  so: 'Sophomore',
+  ju: 'Junior',
+  se: 'Senior',
+  gr: 'Graduate',
+};
+
+/**
+ * TODO: Store in dynamic fashion
+ */
+export const MAJORS = {};
