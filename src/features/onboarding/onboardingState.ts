70138ec57d6161a99116281e0d1ff4618dccfed1
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-03-10 10:31:07 -0600

    chore: Create user onboarding hook
    The useOnboardingState hook is used to encapsulate data tracked during
    user onboarding; that is when the user is setting up the app for the
    first time and initializing their profile.
    
    TODO: Add all necessary functionality to hook

---
+++ src/features/onboarding/onboardingState.ts
@@ -0,0 +1,18 @@
+/**
+ * A hook that keeps track of what aspects of the user have been onboarded.
+ *
+ * TODO: Track what features the user has used to enable nudges later.
+ */
+export function useOnboardingState(): OnboardingState {
+  const isOnboarded = Boolean(localStorage.getItem('onboarded')) || false;
+  return {
+    isOnboarded,
+  };
+}
+
+/**
+ * Information tracked during user onboarding.
+ */
+type OnboardingState = {
+  isOnboarded: boolean;
+};
