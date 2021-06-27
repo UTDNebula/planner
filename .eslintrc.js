e25eba9d440dd9306354b71fd22e79b9e2a60b16
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-06-26 19:25:13 -0700

    chore(config): Set up Husky, Prettier, and ESLint to enforce code quality and code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,17 @@ module.exports = {
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
     },
-    project: `./tsconfig.json`,
+    // project: ['./tsconfig.json'],
   },
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'prettier', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
-    
+
     'react/react-in-jsx-scope': 'off',
   },
   settings: {
