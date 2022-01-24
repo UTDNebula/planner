3d5696fe4b31e5f2f6d89370b5f5ddebe2fbe7ea
wbne <firetruckbw@gmail.com>
2022-01-23 18:33:40 -0600

    feat: Link firebase storage with local redux stores

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { motion } from 'framer-motion';
 import Head from 'next/head';
 import { NoticeBlock } from '../../components/home/announcements/NoticeBlock';
 import UserWelcome from '../../components/home/UserWelcome';
 import UserPlanSheet from '../../components/home/UserPlanSheet';
 import useUserPlanSheetTransition from '../../components/home/userPlanSheetTransition';
+import { updateUser, updateAllUserData } from '../../modules/profile/userDataSlice';
+import { useAuthContext } from '../../modules/auth/auth-context';
+import firebase from 'firebase';
 
 const SHEET_START_ANIMATION = {
   y: 0,
@@ -20,9 +24,36 @@ const SHEET_END_ANIMATION = {
 
 /**
  * The home screen for the app.
+ * If the user has a valid Firebase id, then the data will be loaded.
  */
 export default function Home(): JSX.Element {
   const { sheetIsOpen, togglePlan } = useUserPlanSheetTransition();
+  const { user } = useAuthContext();
+  const [initiallySynced, setSync] = React.useState<boolean>(false);
+  const firestore = firebase.firestore();
+  const dispatch = useDispatch();
+  dispatch(updateUser(user));
+  console.log(user);
+
+  if (user.id !== 'guest' && !initiallySynced) {
+    console.log('uid');
+    const userDoc = firestore
+      .collection('users')
+      .doc(user.id)
+      .get()
+      .then((userDoc) => {
+        console.log('doc exists');
+        if (userDoc) {
+          const userData = userDoc.data();
+          const userSlice = userData.userDataSlice;
+          dispatch(updateAllUserData(userSlice));
+        }
+      })
+      .catch((error) => {
+        console.log('error: ', error);
+      });
+    setSync(true);
+  }
 
   return (
     <>
