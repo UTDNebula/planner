e5c7f7d79aa04c1f202b2691442073806824dd43
ZzRanger <calebmarklim@gmail.com>
2022-10-11 11:04:50 -0500

    chore: Update banner on profile 2

--- components/newhome/Profile.tsx
+++ components/newhome/Profile.tsx
@@ -1,5 +1,6 @@
 import { TextField } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
+import { purple } from '@mui/material/colors';
 
 import { useAuthContext } from '../../modules/auth/auth-context';
 
@@ -10,19 +11,25 @@ export default function ProfilePage(): JSX.Element {
   const { user } = useAuthContext();
   return (
     <main className="h-screen w-full flex flex-col overflow-scroll">
-      <section className="bg-[#3E61ED] h-40 fixed w-full z-10">
-        <Avatar
-          alt="Remy Sharp"
-          sx={{ width: 120, height: 120, position: 'absolute', bottom: '-60px', left: '20px' }}
-        >
-          MK
-        </Avatar>
-      </section>
-      <section className="mt-40 flex flex-col gap-y-8 self-center items-center">
-        <article className="bg-white">
-          <div className="grid grid-cols-2 gap-x-32 gap-y-16 px-6 py-4">
-            <div className="font-semibold text-[40px] mb-[-40px]">Profile</div>
-            <div></div>
+      <div className="flex flex-col gap-y-4 self-center items-center">
+        <section className="bg-white rounded-2xl">
+          <article className="rounded-t-lg relative bg-gradient-to-r from-purple-500 to-blue-500 h-40 w-full z-10">
+            <Avatar
+              alt="Remy Sharp"
+              sx={{
+                bgcolor: purple[300],
+                width: 120,
+                height: 120,
+                position: 'absolute',
+                bottom: '-20px',
+                left: '20px',
+              }}
+            >
+              MK
+            </Avatar>
+          </article>
+          <article className="grid grid-cols-2 gap-x-32 gap-y-16 px-6 py-4">
+            <div className="font-semibold text-[40px] mb-[-40px] col-span-full">Profile</div>
             <TextField
               name="name"
               id="outlined-basic"
@@ -77,25 +84,25 @@ export default function ProfilePage(): JSX.Element {
               value={'Hi'}
               onChange={() => console.log('HI')}
             />
-          </div>
+          </article>
           <button
             onClick={() => console.log('Hi')}
             className="self-center text-white justify-center items-center w-40 h-12 m-4 flex p-4 bg-[#3E61ED] rounded-2xl"
           >
             Update Profile
           </button>
-        </article>
-        <article className="bg-white flex flex-col w-full px-6 py-4">
+        </section>
+        <section className="bg-white flex flex-col w-full px-6 py-4 rounded-2xl">
           <div className="font-semibold text-[40px]">Delete My Account</div>
-          <div className="text-sm">Deleting your account will remove all user data</div>
+          <div className="text-sm ">Deleting your account will remove all user data</div>
           <button
             onClick={() => console.log('Hi')}
-            className="text-white justify-center items-center w-20 h-8 flex p-4 bg-[#FF0041] rounded-xl"
+            className="mt-5 text-white justify-center items-center w-20 h-8 flex p-4 bg-[#FF0041] rounded-xl"
           >
             Delete
           </button>
-        </article>
-      </section>
+        </section>
+      </div>
     </main>
   );
 }
