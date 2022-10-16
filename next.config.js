bbf60292100b01fd65f3215aeb08468fa9b35924
Kevin G <ge.kevin.a@gmail.com>
2022-10-16 10:48:12 -0500

    perf: Update Next to v12.2, use SWC modularizeImports & bundle analyzer
    BREAKING CHANGE: Update Next v12.1 -> v12.2.6, use SWC modularize imports, add
    bundle analyzer. See:
    https://github.com/UTDNebula/planner/issues/168#issuecomment-1279992963
    
    #168

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
+  experimental: {
+    modularizeImports: {
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/icons-material': {
+        transform: '@mui/icons-material/{{member}}',
+      },
+    },
+  },
+});
+
+module.exports = nextConfig;
