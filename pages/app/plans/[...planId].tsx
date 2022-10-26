1f4873248da56e1848e7b667f5b6709e711632f4
Jason Antwi-Appah <hey@jasonaa.me>
2022-10-26 03:28:51 -0500

    feat: Create degree validation modal

--- pages/app/plans/[...planId].tsx
+++ pages/app/plans/[...planId].tsx
@@ -1,8 +1,8 @@
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { Fab, Theme } from '@mui/material';
+import { CircularProgress, Dialog, Fab, Theme } from '@mui/material';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { makeStyles } from 'tss-react/mui';
 
 import ErrorMessage from '../../../components/common/ErrorMessage';
@@ -126,30 +126,157 @@ export default function PlanDetailPage(): JSX.Element {
       </PlannerContainer>
     </div>
   );
-
+  const validatePlan = () => {
+    setShowValidation(true);
+  };
+  const [showValidation, setShowValidation] = useState(false);
   return (
-    <div className="h-full flex flex-col overflow-x-hidden overflow-y-auto">
-      <div className="flex-none">
-        {err !== undefined && ErrorMessage(err)}
-        <PlanningToolbar
-          setPlanTitle={setTitle}
-          planId={plan.id}
-          sectionIndex={section}
-          planTitle={title}
-          shouldShowTabs={shouldShowTabs}
-          onTabChange={handleTabChange}
-          onExportPlan={() => {
-            console.log('Exporting plan');
-            exportPlan(plan);
-          }}
-          onImportPlan={(event) => {
-            handleSelectedPlanChange(event, updateLoadedPlan);
-          }}
-        />
+    <>
+      <ValidationDialog
+        open={showValidation}
+        onClose={() => setShowValidation(false)}
+        plan={plan}
+      />
+      <div className="flex flex-col h-full overflow-x-hidden overflow-y-auto">
+        <div className="flex-none">
+          {err !== undefined && ErrorMessage(err)}
+          <PlanningToolbar
+            setPlanTitle={setTitle}
+            planId={plan.id}
+            sectionIndex={section}
+            planTitle={title}
+            shouldShowTabs={shouldShowTabs}
+            onTabChange={handleTabChange}
+            onValidate={validatePlan}
+            onExportPlan={() => {
+              console.log('Exporting plan');
+              exportPlan(plan);
+            }}
+            onImportPlan={(event) => {
+              handleSelectedPlanChange(event, updateLoadedPlan);
+            }}
+          />
+        </div>
+        <div className="flex-1">
+          <div className="">{content}</div>
+        </div>
       </div>
-      <div className="flex-1">
-        <div className="">{content}</div>
-      </div>
-    </div>
+    </>
   );
+}
+
+// TODO: Refactor everything below this line into a separate file
+
+const ValidationDialog = (props: { open: boolean; onClose: () => void; plan: StudentPlan }) => {
+  const { open, onClose, plan } = props;
+  const [loading, setLoading] = useState(true);
+  const [outputData, setOutputData] = useState<DVResponse>(null);
+  const ac = new AbortController();
+  useEffect(() => {
+    if (!open) {
+      setLoading(true);
+      return;
+    }
+    const performValidation = async () => {
+      const body: DVRequest = {
+        courses: plan.semesters
+          .flatMap((s) => s.courses)
+          .map((c) => {
+            const split = c.catalogCode.split(' ');
+            const department = split[0];
+            const courseNumber = Number(split[1]);
+            const level = Math.floor(courseNumber / 1000);
+            const hours = Math.floor((courseNumber - level * 1000) / 100);
+            return {
+              name: c.catalogCode,
+              department: department,
+              level,
+              hours,
+            };
+          }),
+        bypasses: [],
+        degree: 'computer_science_ug',
+      };
+      const res = (await (
+        await fetch(`${process.env.NEXT_PUBLIC_VALIDATION_SERVER}/validate-degree-plan`, {
+          method: 'POST',
+          body: JSON.stringify(body),
+          signal: ac.signal,
+          headers: {
+            'content-type': 'application/json',
+          },
+        })
+      ).json()) as DVResponse;
+      setOutputData(res);
+      setLoading(false);
+    };
+    performValidation();
+    return () => ac.abort();
+  }, [open]);
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <div className="p-10">
+        <h1>Degree Validation</h1>
+
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <ul className="list-disc">
+            {Object.entries(outputData).map(([key, value]) => {
+              return (
+                <li className="my-2" key={key}>
+                  <div className="flex justify-between">
+                    <p className="font-bold">{key}</p>{' '}
+                    <p className="text-right">
+                      {value.isfilled ? `Complete` : `Incomplete`} ({value.hours} credits)
+                    </p>
+                  </div>
+                  <ul className="list-disc ml-4">
+                    {Object.keys(value.courses).length != 0 ? (
+                      Object.entries(value.courses).map(([course, credits]) => (
+                        <li key={course}>
+                          {course} - {credits} credits
+                        </li>
+                      ))
+                    ) : (
+                      <li className="italic">No credits planned for this requirement.</li>
+                    )}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </Dialog>
+  );
+};
+
+interface DVRequest {
+  courses: DVCourse[];
+  bypasses: DVBypass[];
+  degree: string;
+}
+
+interface DVResponse {
+  [requirement: string]: DVRequirement;
+}
+
+interface DVRequirement {
+  courses: Record<string, number>;
+  hours: number;
+  isfilled: boolean;
+}
+
+interface DVBypass {
+  course: string;
+  requirement: string;
+  hours: number;
+}
+
+interface DVCourse {
+  name: string;
+  department: string;
+  level: number;
+  hours: number;
 }
