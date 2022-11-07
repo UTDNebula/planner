57281710cf2be7aca46a151bfa4f8db598b092e1
lokeshyerneni <lokeshyerneni0429@gmail.com>
2022-11-06 18:37:12 -0600

    feat(link): Added the google forms link to "report a bug" section

--- components/BetaBanner.tsx
+++ components/BetaBanner.tsx
@@ -5,7 +5,13 @@ const BetaBanner: FC = () => {
     <div className="w-full h-[50px] flex items-center justify-center gap-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <div className="flex gap-3 items-center">
         <h3 className="text-white text-[18px] font-medium">{'Hey! This is our beta release'}</h3>
-        <button className="bg-white hover:bg-gray-50 rounded-md px-2 py-1 flex items-center justify-center font-medium">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            window.open('https://forms.gle/wx4zxm3cemut31tNA', '_blank');
+          }}
+          className="bg-white hover:bg-gray-50 rounded-md px-2 py-1 flex items-center justify-center font-medium"
+        >
           Report a bug
         </button>
       </div>
