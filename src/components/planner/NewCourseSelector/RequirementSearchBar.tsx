40b4c21e3dafb6c699a3e8304a92239cfebab91c
ZzRanger <calebmarklim@gmail.com>
2022-12-21 22:47:24 -0600

    fix SearchBar being squished during animation

--- src/components/planner/NewCourseSelector/RequirementSearchBar.tsx
+++ src/components/planner/NewCourseSelector/RequirementSearchBar.tsx
@@ -19,7 +19,7 @@ export default function SearchBar({ updateQuery }: SearchBarProps): JSX.Element 
   };
 
   return (
-    <div className="rounded-md border-[#1C2A6D] border flex flex-row pl-5 pr-2 h-[30px] justify-between items-center">
+    <div className="rounded-md border-[#1C2A6D] border flex flex-row pl-5 pr-2 min-h-[30px] justify-between items-center">
       <input
         className="outline-none text-xs"
         placeholder="Filter courses"
