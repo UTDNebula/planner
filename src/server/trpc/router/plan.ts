5e16ba3130e33515febcbce81caee225278c2da9
Sarthak Gaur <sg@pop-os.localdomain>
2023-01-03 18:32:46 -0600

    added plan delete and semester delete endpoint

--- src/server/trpc/router/plan.ts
+++ src/server/trpc/router/plan.ts
@@ -15,8 +15,10 @@ export const planRouter = router({
           },
           select: {
             name: true,
+            id: true,
             semesters: {
               select: {
+                id: true,
                 name: true,
                 code: true,
                 courses: {
@@ -39,4 +41,75 @@ export const planRouter = router({
     }
     return planData.plans[0];
   }),
+  deletePlanById: protectedProcedure.input(z.string().min(1)).mutation(async ({ ctx, input }) => {
+    // check if plans belongs to user with id = ctx.session.user.id
+    try {
+      const planData = await ctx.prisma.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+        select: {
+          plans: {
+            where: {
+              id: input,
+            },
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+      const plan = planData?.plans[0];
+      await ctx.prisma.plan.delete({
+        where: {
+          id: plan?.id,
+        },
+      });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }),
+  deleteSemesterById: protectedProcedure
+    .input(
+      z.object({
+        planId: z.string().min(1),
+        semesterId: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const semesterId = await ctx.prisma.user.findUnique({
+          where: {
+            id: ctx.session.user.id,
+          },
+          select: {
+            plans: {
+              where: {
+                id: input.planId,
+              },
+              select: {
+                id: true,
+                semesters: {
+                  where: {
+                    id: input.semesterId,
+                  },
+                  select: {
+                    id: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+        await ctx.prisma.semester.delete({
+          where: {
+            id: semesterId?.plans[0]?.semesters[0]?.id,
+          },
+        });
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }),
 });
