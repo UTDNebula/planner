90bc872a33f6f4a62d5d3fd65aea382209707f5d
Sarthak Gaur <sg@pop-os.localdomain>
2023-01-03 19:07:25 -0600

    ssr for homepage

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -1,19 +1,33 @@
-
 import * as React from 'react';
-
-import Credits from '../../components/newhome/Credits';
 import Home from '../../components/newhome/Home';
 import HomeDrawer from '../../components/newhome/HomeDrawer';
-import Profile from '../../components/newhome/Profile';
 import useMedia from '../../modules/common/media';
 
+import superjson from 'superjson';
+import { createProxySSGHelpers } from '@trpc/react-query/ssg';
+
+import { createContextInner } from '@server/trpc/context';
+import { appRouter } from '@/server/trpc/router/_app';
+import { GetServerSidePropsContext } from 'next';
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]';
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await unstable_getServerSession(context.req, context.res, authOptions);
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContextInner({ session }),
+    transformer: superjson,
+  });
+
+  await ssg.plan.getUserPlans.prefetch();
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+  };
+}
 export default function MiniDrawer() {
   const isDesktop = useMedia('(min-width: 900px)');
-  const content = [
-    <Home key={0} />,
-    <Profile isDesktop={isDesktop} key={1} />,
-    <Credits key={2} />,
-  ];
 
   return (
     <>
