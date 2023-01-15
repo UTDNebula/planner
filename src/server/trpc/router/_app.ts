fdf5740e0f03796df05af1cbe1c3ea09e7478823
ZzRanger <calebmarklim@gmail.com>
2023-01-15 15:15:05 -0600

    add credits server route

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,4 +1,5 @@
 import { router } from '../trpc';
+import { creditsRouter } from './credits';
 import { planRouter } from './plan';
 import { templateRouter } from './template';
 import { userRouter } from './user';
@@ -6,6 +7,7 @@ export const appRouter = router({
   user: userRouter,
   plan: planRouter,
   template: templateRouter,
+  credits: creditsRouter,
 });
 
 // export type definition of API
