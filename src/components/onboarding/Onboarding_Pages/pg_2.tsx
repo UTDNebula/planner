c94e7a5d69e413b45edbdef865f619cfda6fe3d5
ZzRanger <calebmarklim@gmail.com>
2023-01-27 11:15:39 -0600

    change pg_2 to credits add

--- src/components/onboarding/Onboarding_Pages/pg_2.tsx
+++ src/components/onboarding/Onboarding_Pages/pg_2.tsx
@@ -1,340 +1,151 @@
-import { FormControlLabel, FormGroup } from '@mui/material';
-import Autocomplete from '@mui/material/Autocomplete';
-import Checkbox from '@mui/material/Checkbox';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import React from 'react';
+import DataGrid from '@/components/credits/DataGrid';
+import DropdownSelect from '@/components/credits/DropdownSelect';
+import { Credit } from '@/components/credits/types';
+import { displaySemesterCode } from '@/components/planner/Tiles/SemesterTile';
+import { generateSemesters } from '@/modules/common/data';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import React, { useMemo, useState } from 'react';
 
-import DummyData from '../../../data/dummy_onboarding.json';
-import Degrees from '../../../data/majors.json';
-import { HONORS_INDICATOR_LABELS } from '../../../modules/common/data-utils';
-import { HonorsIndicator } from '../../../modules/common/types';
+import { SemesterCode, SemesterType } from '@prisma/client';
+import useSearch from '@/components/search/search';
+import { loadDummyCourses } from '@/utils/utilFunctions';
+import SearchBar from '@/components/credits/SearchBar';
+import { FormControlLabel, Switch } from '@mui/material';
+import AutoCompleteSearchBar from '@/components/credits/AutoCompleteSearchBar';
+import Button from '@/components/credits/Button';
 
 // Array of values to choose from for form
-const scholarships = DummyData.scholarships;
-
-const fastTrackYears = DummyData.fastTrackYears;
-const majors = Degrees;
 
 export type PageTwoTypes = {
-  scholarship: boolean;
-  scholarshipType: string; // ScholarshipType;
-  receivingAid: boolean;
-  fastTrack: boolean;
-  fastTrackMajor: string;
-  fastTrackYear: string;
-  honors: HonorsIndicator[];
+  credits: Credit[];
 };
 
-/**
- * Renders a list of MenuItem options for the user to select in the dropdowns.
- *
- * @param array An array of any type where the indices are rendered as separate options
- * @return The rendered list of MenuItems
- */
-function returnMenuItems<MenuItem>(menuOptions: string[]) {
-  // TODO: Place in a utils file
-  return menuOptions.map((option) => (
-    <MenuItem key={option} value={option}>
-      {option}
-    </MenuItem>
-  ));
-}
-
-export type Page2Props = {
+export type Page2data = {
   handleChange: React.Dispatch<React.SetStateAction<PageTwoTypes>>;
-  props: PageTwoTypes;
+  data: PageTwoTypes;
   handleValidate: (value: boolean) => void;
 };
 
-export default function PageTwo({ handleChange, props, handleValidate }: Page2Props): JSX.Element {
-  const {
-    scholarship,
-    scholarshipType,
-    receivingAid,
-    fastTrack,
-    fastTrackMajor,
-    fastTrackYear,
-    honors,
-  } = props;
+export default function PageTwo({ handleChange, data, handleValidate }: Page2data): JSX.Element {
+  const { credits } = data;
 
-  const honorsCheck: { [key: string]: boolean } = {
-    cv: false,
-    cs2: false,
-    lahc: false,
-    bbs: false,
-    ah: false,
-    epps: false,
-    nsm: false,
-    atec: false,
-  };
+  // TODO: Change start semester to when they first joined UTD - 2
+  const semesters = useMemo(
+    () =>
+      generateSemesters(8, new Date().getFullYear() - 4, SemesterType.f)
+        .reverse()
+        .map((sem) => sem.code),
+    [],
+  );
 
-  for (const val of honors) {
-    const tempKeys = Object.keys(honorsCheck);
-    if (tempKeys.includes(val)) {
-      honorsCheck[val as string] = true;
-    }
-  }
+  const [semesterCode, setSemester] = useState<SemesterCode>(semesters[0]);
 
-  const checkValidate = () => {
-    const isPrimaryValid =
-      scholarship !== null && receivingAid !== null && fastTrack !== null ? true : false;
-    const scholarshipTypeValid = (scholarship && scholarshipType) || !scholarship;
-    const fastTrackValid = (fastTrack && fastTrackMajor && fastTrackYear) || !fastTrack;
-    const valid = isPrimaryValid && scholarshipTypeValid && fastTrackValid ? true : false;
-    handleValidate(valid);
-  };
-
-  // Handles change for Select
-  const handleStandardChange = (event: SelectChangeEvent<string>) => {
-    handleChange({ ...props, [event.target.name]: event.target.value });
-  };
-
-  // Handles change for Button
-  const handleButtonChange = (
-    _event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    buttonName: string,
-    value: boolean,
-    ...clearValues: string[]
-  ) => {
-    const clear: { [key: string]: string } = {};
-    for (const val of clearValues) {
-      clear[val] = '';
-    }
-
-    handleChange({ ...props, [buttonName]: value, ...clear });
-  };
-
-  // Handles change for Autocomplete
-  const handleAutocompleteChange = (event: React.SyntheticEvent<Element>, value: string | null) => {
-    handleChange({
-      ...props,
-      fastTrackMajor: value ?? '',
-    });
-  };
-
-  // Handles change for Checkboxes
-  const handleCheckChange = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
-    const temp: HonorsIndicator[] = checked
-      ? [...honors, event.target.name as HonorsIndicator]
-      : honors.filter((value) => value !== event.target.name);
-    handleChange({
-      ...props,
-      honors: temp,
-    });
-  };
-
-  React.useEffect(() => {
-    checkValidate();
+  const { results, updateQuery } = useSearch({
+    getData: loadDummyCourses,
+    initialQuery: '',
+    filterFn: (course, query) => course.code.toLowerCase().includes(query.toLowerCase()),
   });
 
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+  const [courseCode, setCourseCode] = useState('');
+  const [transfer, setTransfer] = useState(true);
+
+  const handleAddCredit = () => {
+    if (courseCode) {
+      // Check for duplicates
+      if (!credits.some((credit) => credit.courseCode === courseCode))
+        handleChange({
+          credits: [...credits, { courseCode: courseCode, transfer, semesterCode: semesterCode }],
+        });
+    }
+  };
+
+  const handleRemoveCredit = (removeCredit: Credit) => {
+    handleChange({
+      credits: credits.filter((credit) => credit !== removeCredit),
+    });
+  };
 
   return (
-    <div className="animate-intro w-full">
-      <h2 className="text-4xl text-left font-bold mb-10 text-gray-800">
-        <div>Scholarships </div>
-        <div> &#38; Honors</div>{' '}
-      </h2>
-      <div className="grid grid-cols-2 gap-x-32">
-        <div>
-          <h3 className="text-xl  text-gray-800 ">Are you receiving any scholarships?</h3>
-          <div className="flex mt-2 mb-6 ">
-            <button
-              onClick={(event) => handleButtonChange(event, 'scholarship', true)}
-              className={`${
-                scholarship ? 'bg-yellow-400' : null
-              }    hover:bg-yellow-400 text-left text-grey-700 text-sm font-medium hover:text-white h-8  px-6 border border-blue-600 hover:border-transparent rounded`}
-            >
-              YES
-            </button>
+    <div className="animate-intro flex flex-col">
+      <div className="text-lg">Add Credits</div>
+      <div>Add credits that you have already taken in your degree plan here!</div>
 
-            <button
-              onClick={(event) => {
-                handleButtonChange(event, 'scholarship', false, 'scholarshipType');
-              }}
-              className={`${
-                scholarship == false ? 'bg-yellow-400' : null
-              }  ml-5 hover:bg-yellow-400 text-left text-grey-700 text-sm font-medium hover:text-white h-8  px-6 border border-blue-600 hover:border-transparent rounded`}
-            >
-              NO
-            </button>
-          </div>
-        </div>
-        <div>
-          {scholarship && (
-            <h3 className="text-xl text-gray-800">What type of scholarship did you receive?</h3>
-          )}
-          {scholarship && (
-            <FormControl variant="outlined">
-              <InputLabel id="demo-simple-select-autowidth-label">Scholarship Type</InputLabel>
-              <Select
-                labelId="demo-simple-select-autowidth-label"
-                id="demo-simple-select-autowidth"
-                value={scholarshipType}
-                onChange={handleStandardChange}
-                name="scholarshipType"
-                label="Type of Scholarship"
-                className="w-72"
-              >
-                {returnMenuItems(scholarships)}
-              </Select>
-            </FormControl>
-          )}
-        </div>
-        <div>
-          <h3 className="text-xl text-gray-800">Are you receiving financial aid?</h3>
-          <div className="flex mt-2 items-center mb-10 ">
-            <button
-              onClick={(event) => handleButtonChange(event, 'receivingAid', true)}
-              className={`${
-                receivingAid ? 'bg-yellow-400' : null
-              }  hover:bg-yellow-400 text-left text-grey-700 text-sm font-medium hover:text-white h-8  px-6 border border-blue-600 hover:border-transparent rounded`}
-            >
-              YES
-            </button>
-            <button
-              onClick={(event) => handleButtonChange(event, 'receivingAid', false)}
-              className={`${
-                receivingAid == false ? 'bg-yellow-400' : null
-              }  ml-5 hover:bg-yellow-400 text-left text-grey-700 text-sm font-medium hover:text-white h-8  px-6 border border-blue-600 hover:border-transparent rounded`}
-            >
-              NO
-            </button>
-          </div>
-        </div>
-        <div></div>
-        <div>
-          <h3 className="text-xl text-gray-800">
-            Are you in / plan on enrolling in the fast track program?
-          </h3>
-          <div className="flex mt-2 items-center mb-10 ">
-            <button
-              onClick={(event) => handleButtonChange(event, 'fastTrack', true)}
-              className={`${
-                fastTrack ? 'bg-yellow-400' : null
-              } hover:bg-yellow-400 text-left text-grey-700 text-sm font-medium hover:text-white h-8  px-6 border border-blue-600 hover:border-transparent rounded`}
-            >
-              YES
-            </button>
+      <div className="w-72">
+        <AutoCompleteSearchBar
+          onValueChange={(value) => setCourseCode(value)}
+          onInputChange={(query: string) => updateQuery(query)}
+          options={results.map((course) => course.code)}
+          style={{ maxWidth: '450px', minWidth: '350px' }}
+          autoFocus
+        />
+      </div>
 
-            <button
-              onClick={(event) => {
-                handleButtonChange(event, 'fastTrack', false, 'fastTrackMajor', 'fastTrackYear');
-              }}
-              className={`${
-                fastTrack == false ? 'bg-yellow-400' : null
-              }  ml-5 hover:bg-yellow-400 text-left text-grey-700 text-sm font-medium hover:text-white h-8  px-6 border border-blue-600 hover:border-transparent rounded`}
-            >
-              NO
-            </button>
-          </div>
-        </div>
-        <div>
-          {fastTrack && <h3 className="text-xl mb-4 text-gray-800">For what major and year?</h3>}
-          {fastTrack && (
-            <div className="mb-4 max-w-lg h-32 w-84 rounded-lg shadow-lg border border-blue-600 p-4 ">
-              <div className="mb-2">
-                <Autocomplete
-                  size={'small'}
-                  value={fastTrackMajor}
-                  className="w-72"
-                  onChange={handleAutocompleteChange}
-                  id="combo-box-demo"
-                  options={majors}
-                  style={{ width: 290 }}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Major" variant="outlined" />
-                  )}
-                />
-              </div>
+      <div className="flex flex-row">
+        <DropdownSelect
+          id="semester"
+          value={semesterCode}
+          values={semesters as (SemesterCode & { [key: string]: string })[]}
+          getValue={(semester) => semester}
+          getDisplayedValue={(semester) => displaySemesterCode(semester)}
+          onChange={(sem) => setSemester(sem)}
+        />
+        <FormControlLabel
+          control={<Switch defaultChecked onChange={() => setTransfer(!transfer)} />}
+          label="Label"
+        />
+      </div>
 
-              <div className="">
-                <FormControl className="w-32">
-                  <InputLabel id="demo-simple-select-autowidth-label">Year</InputLabel>
-                  <Select
-                    label="year"
-                    labelId="demo-simple-select-autowidth-label"
-                    id="demo-simple-select-autowidth"
-                    value={fastTrackYear}
-                    onChange={handleStandardChange}
-                    name="fastTrackYear"
-                    fullWidth={true}
-                  >
-                    {returnMenuItems(fastTrackYears)}
-                  </Select>
-                </FormControl>
-              </div>
-            </div>
-          )}
-        </div>
-        <div>
-          <h3 className="text-xl mb-2  text-gray-800">Are you in any honors programs?</h3>
+      <Button onClick={handleAddCredit}>{'Add Credit'}</Button>
 
-          <FormControl margin="none" component="fieldset">
-            <FormLabel component="legend">Select all that apply</FormLabel>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox checked={honorsCheck['cv']} onChange={handleCheckChange} name="cv" />
-                }
-                label={HONORS_INDICATOR_LABELS['cv']}
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox checked={honorsCheck['cs2']} onChange={handleCheckChange} name="cs2" />
-                }
-                label={HONORS_INDICATOR_LABELS['cs2']}
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={honorsCheck['lahc']}
-                    onChange={handleCheckChange}
-                    name="lahc"
-                  />
-                }
-                label={HONORS_INDICATOR_LABELS['lahc']}
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox checked={honorsCheck['bbs']} onChange={handleCheckChange} name="bbs" />
-                }
-                label={HONORS_INDICATOR_LABELS['bbs']}
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox checked={honorsCheck['ah']} onChange={handleCheckChange} name="ah" />
-                }
-                label={HONORS_INDICATOR_LABELS['ah']}
-              />
-            </FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox checked={honorsCheck['epps']} onChange={handleCheckChange} name="epps" />
-              }
-              label={HONORS_INDICATOR_LABELS['epps']}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={honorsCheck['nsm']} onChange={handleCheckChange} name="nsm" />
-              }
-              label={HONORS_INDICATOR_LABELS['nsm']}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={honorsCheck['atec']} onChange={handleCheckChange} name="atec" />
-              }
-              label={HONORS_INDICATOR_LABELS['atec']}
-            />
-          </FormControl>
-        </div>
+      <div className="gap-2 px-2 py-4 max-h-[400px]">
+        <DataGrid
+          columns={[
+            {
+              title: 'Course Number',
+              key: 'courseCode',
+              valueGetter: (credit) => credit.courseCode,
+            },
+            {
+              title: 'Transfer',
+              valueGetter: (credit) => (!credit.transfer ? 'Yes' : 'No'),
+            },
+            {
+              title: 'Semester',
+              valueGetter: (credit) =>
+                credit.semesterCode
+                  ? `${displaySemesterCode(credit.semesterCode)}`
+                  : 'Transferred in',
+            },
+          ]}
+          childrenProps={{
+            headerProps: {
+              style: {
+                padding: '20px 0',
+              },
+            },
+            gridProps: {
+              style: {},
+            },
+            rowProps: {
+              style: {
+                borderTop: '1px solid #DEDFE1',
+                padding: '20px 0',
+              },
+              injectedComponent: {
+                Element: () => (
+                  <DeleteIcon className="text-red-500 cursor-pointer absolute right-5 top-1/2 -translate-y-1/2" />
+                ),
+                onClick: (_, row) => {
+                  handleRemoveCredit(row);
+                },
+              },
+            },
+          }}
+          rows={[...credits].reverse()}
+          RowCellComponent={({ children }) => <span className="text-black">{children}</span>}
+          TitleComponent={({ children }) => <h4 className="text-black">{children}</h4>}
+          LoadingComponent={() => <h2 className="text-black">Loading...</h2>}
+        />
       </div>
     </div>
   );
