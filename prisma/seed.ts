f96381e91dd0bf3fcaebe1f5d2038a18272ae3e7
Sarthak Gaur <sarthak22gaur@gmail.com>
2023-01-28 20:54:15 -0600

    db seed for prod

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient, TemplateDataType } from '@prisma/client';
+import { PrismaClient, TemplateDataType } from '@prisma/client';
 
 import dummyTemplate from '../src/data/degree_template.json';
 const prisma = new PrismaClient();
@@ -18,41 +18,49 @@ async function main() {
       obj[key] = annoyed[key];
       return obj;
     }, {});
-  const deletedTemplates = await prisma.template.deleteMany({});
-  console.log(`Deleted ${deletedTemplates.count} templates`);
-  for (const [key, value] of Object.entries(orderedTemplate)) {
-    const template = await prisma.template.create({
-      data: {
-        name: key,
-      },
-    });
-    for (let i = 0; i < value.length; i++) {
-      let tData: Prisma.TemplateDataCreateInput;
-      const templateDataItems: Array<TemplateDataItems> = [];
-      for (let j = 0; j < value[i].length; j++) {
-        if (typeof value[i][j] === 'object') {
-          templateDataItems.push({
-            name: (value[i][j] as { options: string }).options + ' Course',
-            type: 'OPTIONAL',
-          });
-        } else {
-          templateDataItems.push({ name: value[i][j] as string, type: 'CORE' });
-        }
-      }
-      await prisma.templateData.create({
+
+  const templatesFromDb = await prisma.template.findMany();
+  let templatesToAdd: Array<string> = [];
+  if (templatesFromDb.length !== Object.keys(orderedTemplate).length) {
+    templatesToAdd = Object.keys(orderedTemplate).filter(
+      (item) => !templatesFromDb.some((dbItem) => dbItem.name === item),
+    );
+  }
+
+  if (templatesToAdd.length > 0) {
+    for (const key of templatesToAdd) {
+      const value = orderedTemplate[key];
+      const template = await prisma.template.create({
         data: {
-          semester: i + 1,
-          items: {
-            create: [...templateDataItems],
-          },
-          template: {
-            connect: { id: template.id },
-          },
+          name: key,
         },
       });
-      console.log(`Created template data for ${key} semester ${i + 1}`);
+      for (let i = 0; i < value.length; i++) {
+        const templateDataItems: Array<TemplateDataItems> = [];
+        for (let j = 0; j < value[i].length; j++) {
+          if (typeof value[i][j] === 'object') {
+            templateDataItems.push({
+              name: (value[i][j] as { options: string }).options + ' Course',
+              type: 'OPTIONAL',
+            });
+          } else {
+            templateDataItems.push({ name: value[i][j] as string, type: 'CORE' });
+          }
+        }
+        await prisma.templateData.create({
+          data: {
+            semester: i + 1,
+            items: {
+              create: [...templateDataItems],
+            },
+            template: {
+              connect: { id: template.id },
+            },
+          },
+        });
+      }
+      console.log(`Created template ${key}`);
     }
-    console.log(`Created template ${key}`);
   }
 }
 
