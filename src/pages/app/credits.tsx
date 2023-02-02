1b06de0693d1dadbe110d4b7bf654aed1c671bc8
Jason Antwi-Appah <hey@jasonaa.me>
2023-02-02 01:51:19 -0600

    pls

--- src/pages/app/credits.tsx
+++ src/pages/app/credits.tsx
@@ -5,11 +5,12 @@ import { unstable_getServerSession } from 'next-auth';
 import * as React from 'react';
 import superjson from 'superjson';
 
-import Credits from '@/components/home/Credits';
+// import Credits from '@/components/home/Credits';
 import { appRouter } from '@/server/trpc/router/_app';
 
 import useMedia from '../../utils/media';
 import { authOptions } from '../api/auth/[...nextauth]';
+import dynamic from 'next/dynamic';
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await unstable_getServerSession(context.req, context.res, authOptions);
   const ssg = createProxySSGHelpers({
@@ -27,6 +28,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 }
 export default function MiniDrawer() {
   const isDesktop = useMedia('(min-width: 900px)');
+  const Credits = dynamic(() => import('@/components/home/Credits'), { ssr: false });
 
   return <Credits />;
 }
