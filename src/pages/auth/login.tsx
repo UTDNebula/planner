8537aa72f86d0045f450b1247d36f043f0331c97
singharpt <104125829+singharpt@users.noreply.github.com>
2023-02-01 19:53:23 -0600

    fixed bug 373 which hides OR when no provider present

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -60,13 +60,14 @@ export default function AuthPage({
                 </div>
                 <button
                   onClick={handleEmailSignIn}
-                  className="w-full text-center bg-blue-700 hover:bg-blue-800 rounded-lg text-white py-3 font-medium"
+                  className="w-full text-center bg-blue-700 hover:bg-blue-800 text-lg rounded-lg text-white py-3"
                 >
                   Sign in
                 </button>
-                <div className="items-center border-2 border-red-500 mx-auto -mb-6 pb-1">
-                  <h1 className="text-center text-s text-gray-700">or</h1>
-                </div>
+                {providers && 
+                  <div className="items-center border-2 border-red-400 mx-auto rounded-lg -mb-6 pb-1">
+                    <h4 className="text-center text-s text-gray-700">or</h4>
+                  </div>}
                 {providers &&
                   Object.values(providers).map((provider, idx) => (
                     <button
@@ -74,20 +75,20 @@ export default function AuthPage({
                       onClick={() => signIn(provider.id)}
                       className="appearance-none items-center justify-center block w-full bg-gray-100 text-gray-700 shadow border border-gray-500 rounded-lg py-3 px-3 leading-tight hover:bg-gray-200 hover:text-gray-700 focus:outline-none"
                     >
-                      <h1 className="text-center text-xl text-blue-700">
+                      <h4 className="text-center text-lg text-blue-700">
                         Sign in with {provider.name}
-                      </h1>
+                      </h4>
                     </button>)
                   )}
                 <div className="flex place-content-center">
-                  <p>
+                  <h4 className='text-lg'>
                     New to Nebula?
                     <Link legacyBehavior href="/auth/signup">
-                      <a className="ml-2 text-blue-700 font-semibold hover:bg-blue-200 hover:rounded-lg">
+                      <a className="ml-2 text-blue-700 text-lg font-semibold hover:bg-blue-200 hover:rounded-lg">
                         Sign Up
                       </a>
                     </Link>
-                  </p>
+                  </h4>
                 </div>
               </section>
             </div>
