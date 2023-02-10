8d839383f0e2aa58f34c6ad4d830624243d598aa
ZzRanger <calebmarklim@gmail.com>
2023-02-10 13:51:08 -0600

    add prefix requirement

--- major/tests/test_shared_requirements.py
+++ major/tests/test_shared_requirements.py
@@ -4,7 +4,8 @@ from requirements import (
     OrRequirement,
     HoursRequirement,
     FreeElectiveRequirement,
-    SelectRequirement
+    SelectRequirement,
+    PrefixRequirement
 )
 import json
 
@@ -185,3 +186,13 @@ def test_select_requirement() -> None:
     select_req.attempt_fulfill("ECON 2301") 
     assert select_req.fulfilled_count == 2
     assert select_req.is_fulfilled() == True
+
+def test_prefix_requirement() -> None:
+    prefix_req = PrefixRequirement('CS')
+    assert prefix_req.filled == False
+
+    prefix_req.attempt_fulfill('BCOM 1000')
+    assert prefix_req.filled == False
+
+    prefix_req.attempt_fulfill('CS 1336')
+    assert prefix_req.filled == True   
--- utils/__init__.py
+++ utils/__init__.py
@@ -14,4 +14,12 @@ def get_level_from_course(course: str) -> int:
         raise ValueError("Second digit of course code must be valid digit")
 
     return int(code[0]) 
- 
+
+def get_course_prefix(course: str) -> str:
+    [prefix, _] = course.split(" ")
+
+    # In the future, run this against a map of prefixes
+    if not prefix:
+        raise ValueError("Invalid prefix")
+
+    return prefix    
