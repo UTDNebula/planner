b0291e1e9cb8f016fe156d6ede142cf66251d8e7
Kevin G <ge.kevin.a@gmail.com>
2023-02-12 15:55:12 -0600

    Fix degree data issues

--- major/tests/test_degrees.py
+++ major/tests/test_degrees.py
@@ -1,26 +1,17 @@
 from requirements import REQUIREMENTS_MAP
 import json
+import os
 
 
-# just make sure none of the matchers throw an error
-def test_computer_science() -> None:
-    data = json.loads(open("./degree_data/computer_science.json", "r").read())
+# ensure degree data's are valid
+def test_degrees() -> None:
+    for file in os.scandir("./degree_data"):
+        data = json.loads(open(file, "r").read())
 
-    requirements = data["requirements"]["major"]
+        requirements = data["requirements"]["major"]
 
-    # make sure all requirements are parseable
-    for requirement in requirements:
-        REQUIREMENTS_MAP[requirement["matcher"]].from_json(requirement)
-
-
-def test_accounting() -> None:
-    data = json.loads(open("./degree_data/accounting.json", "r").read())
-
-    requirements = data["requirements"]["major"]
-
-    # make sure all requirements are parseable
-    for requirement in requirements:
-        REQUIREMENTS_MAP[requirement["matcher"]].from_json(requirement)
+        for requirement in requirements:
+            REQUIREMENTS_MAP[requirement["matcher"]].from_json(requirement)
 
 
 """Add tests for:
