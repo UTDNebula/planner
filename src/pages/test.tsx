f5d03fe6df68dcb07690e019ca43eebbddc6d379
ZzRanger <calebmarklim@gmail.com>
2023-02-15 16:36:15 -0600

    update req types

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,5 +1,3 @@
-import reqOutput from '@data/test_degree.json';
-
 export type DegreeRequirements = {
   can_graduate: boolean;
   requirements: DegreeRequirement[];
@@ -37,26 +35,62 @@ export type FreeElectiveRequirementGroup = Requirement & {
 };
 
 export type CSGuidedElectiveGroup = Requirement & {
-  matcher: 'CS Guided Elective';
+  matcher: 'CS Guided Electives';
   required_count: number;
   also_fulfills: CourseRequirement[];
   starts_with: string;
   fulfilled_count: number;
   valid_courses: string[];
 };
+
+export type BAGuidedElectives = Requirement & {
+  matcher: 'BA General Business Electives';
+  required_count: number;
+  required_hours: number;
+  fulfilled_count: number;
+  fulfilled_hours: number;
+  valid_courses: string[];
+  prefix_groups: RequirementTypes[];
+};
 // type Requirement = { matcher: string; filled: boolean };
 
 export type CourseRequirement = Requirement & {
-  matcher: 'course';
+  matcher: 'Course';
   course: string;
 };
 
+export type HoursRequirement = Requirement & {
+  matcher: 'Hours';
+  required_hours: number;
+  fulfilled_hours: number;
+  requirements: RequirementTypes[];
+  valid_courses: RequirementTypes[];
+};
+
+/**
+ * RequirementGroups are groups of requirements. A DegreeRequirement is made up of multiple RequirementGroups
+ * i.e. Major Preparatory Courses, Free Electives
+ */
 export type RequirementGroupTypes =
   | AndRequirementGroup
   | FreeElectiveRequirementGroup
   | CSGuidedElectiveGroup;
 
-export type RequirementTypes = CourseRequirement | OrRequirement | AndRequirementGroup;
+export type RequirementTypes =
+  | SelectRequirement
+  | CourseRequirement
+  | OrRequirement
+  | AndRequirementGroup
+  | HoursRequirement
+  | PrefixBucketRequirement
+  | BAGuidedElectives;
+
+export type SelectRequirement = Requirement & {
+  matcher: 'Select';
+  required_course_count: number;
+  fulfilled_count: number;
+  requirements: RequirementTypes[];
+};
 
 export type OrRequirement = Requirement & {
   matcher: 'Or';
@@ -65,135 +99,8 @@ export type OrRequirement = Requirement & {
   num_fulfilled_requirements: number;
 };
 
-export default function Test() {
-  const data: DegreeRequirements = reqOutput as DegreeRequirements;
-  return (
-    <div className="flex w-[300px] flex-col items-center border-2">
-      <div className="text-xl">Your Requirements</div>
-      {data.requirements.map((degreeReq, idx) => (
-        <AccordianWrapper name={degreeReq.name} key={idx}>
-          <DegreeRequirement degreeReq={degreeReq} />
-        </AccordianWrapper>
-      ))}
-    </div>
-  );
-}
-
-/**
- * DegreeRequirement component displays a unique degree requirement at UTD
- *  i.e. Core Curriculum, Computer Science Major
- * @param param0
- * @returns
- */
-function DegreeRequirement({ degreeReq }: { degreeReq: DegreeRequirement }) {
-  return (
-    <div className=" w-full rounded-md border-2 border-black">
-      {degreeReq.requirements ? (
-        degreeReq.requirements.map((reqTest, idx) => (
-          <AccordianWrapper name={reqTest.metadata.name} key={idx}>
-            <RequirementGroup reqGroup={reqTest} />
-          </AccordianWrapper>
-        ))
-      ) : (
-        <div>NOT SUPPORTED</div>
-      )}
-    </div>
-  );
-}
-
-function CSGuidedElectiveComponent({ req }: { req: CSGuidedElectiveGroup }) {
-  return (
-    <div>
-      Completed Courses:
-      {req.valid_courses.map((course, idx) => (
-        <div key={idx}>{course}</div>
-      ))}
-      Select Courses:
-      {req.also_fulfills.map((course, idx) => (
-        <CourseRequirement key={idx} req={course} />
-      ))}
-    </div>
-  );
-}
-/**
- * Group of Requirements
- * i.e. Major Preparatory Courses, Free Electives
- * @param param0
- * @returns
- */
-function RequirementGroup({ reqGroup }: { reqGroup: RequirementGroupTypes }) {
-  const getRequirementGroup = () => {
-    switch (reqGroup.matcher) {
-      case 'And':
-        return reqGroup.requirements.map((req, idx) => (
-          <RecursiveRequirementGroup key={idx} req={req} />
-        ));
-      case 'FreeElectives':
-        return <div>Free Elective</div>;
-      case 'CS Guided Elective':
-        return <CSGuidedElectiveComponent req={reqGroup} />;
-    }
-  };
-
-  return <div>{getRequirementGroup()}</div>;
-}
-
-/**
- * Group of requirements that's recursive?
- * @param param0
- * @returns
- */
-function RecursiveRequirementGroup({ req }: { req: RequirementTypes }) {
-  const getRequirement = () => {
-    switch (req.matcher) {
-      case 'course':
-        return <CourseRequirement req={req} />;
-      case 'Or':
-        return (
-          <div className="flex flex-col">
-            <AccordianWrapper name={req.matcher}>
-              {req.requirements.map((req2, idx) => (
-                <RecursiveRequirementGroup key={idx} req={req2} />
-              ))}
-            </AccordianWrapper>
-          </div>
-        );
-      case 'And':
-        return (
-          <div className="flex flex-col">
-            <AccordianWrapper name={req.matcher}>
-              {req.requirements.map((req2, idx) => (
-                <RecursiveRequirementGroup key={idx} req={req2} />
-              ))}
-            </AccordianWrapper>
-          </div>
-        );
-      default:
-        return <div>NOT SUPPORTED</div>;
-    }
-  };
-  return <>{getRequirement()}</>;
-}
-
-function CourseRequirement({ req }: { req: CourseRequirement }) {
-  return (
-    <div>
-      <div>{req.course}</div>
-    </div>
-  );
-}
-
-/**
- * TODO: Make this custom because it's causing annoying behaviors
- * @param param0
- * @returns
- */
-function AccordianWrapper({ name, children }: { name: string; children: any }) {
-  return (
-    <div className="collapse-arrow collapse" tabIndex={0}>
-      <input type="checkbox" className="border-32 border-orange-500" />
-      <div className="collapse-title">{name}</div>
-      <div className="collapse-content">{children}</div>
-    </div>
-  );
-}
+export type PrefixBucketRequirement = Requirement & {
+  matcher: 'Prefix';
+  valid_courses: string[];
+  prefix: string;
+};
