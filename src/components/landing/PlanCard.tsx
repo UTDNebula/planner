90961a703577e0be033d39ab0bc620534f67e087
JCGarzaa <jgarza0127@yahoo.com>
2023-02-24 17:14:04 -0600

    Formatting

--- src/components/landing/PlanCard.tsx
+++ src/components/landing/PlanCard.tsx
@@ -15,35 +15,49 @@ export default function PlanCard({ id, name, major }: PlanCardProps) {
     router.push(`/app/plans/${id}`);
   };
 
-  const utils = trpc.useContext()
+  const utils = trpc.useContext();
   const deletePlan = trpc.plan.deletePlanById.useMutation({
     async onSuccess() {
-      await utils.plan.invalidate()
-    }
-  })
+      await utils.plan.invalidate();
+    },
+  });
 
   return (
     <>
-    <div className="modal" id="my-modal-2">
-      <div className="modal-box">
-        <h3 className="font-bold text-lg">Delete Plan</h3>
-        <p className="py-4">Are you sure you would like to delete this plan?</p>
-        <div className="modal-action">
-        <a href="#" className="btn alert-error" onClick={() => {deletePlan.mutateAsync(id)}}>Yes</a>
-        <a href="#" className="btn">No</a>
+      <div className="modal" id="my-modal-2">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Delete Plan</h3>
+          <p className="py-4">Are you sure you would like to delete this plan?</p>
+          <div className="modal-action">
+            <a
+              href="#"
+              className="alert-error btn"
+              onClick={() => {
+                deletePlan.mutateAsync(id);
+              }}
+            >
+              Yes
+            </a>
+            <a href="#" className="btn">
+              No
+            </a>
+          </div>
         </div>
       </div>
-    </div>
       <button
         onClick={handlePlanClick}
         className="flex h-[150px] w-full max-w-[300px] flex-col rounded-2xl bg-white py-6 px-8 text-left shadow-2xl transition-all hover:scale-110"
       >
-        <div className='flex flex-row justify-between w-full'>
+        <div className="flex w-full flex-row justify-between">
           <h4 className="max-w-full overflow-hidden text-ellipsis whitespace-nowrap">{name}</h4>
           <div className="dropdown pl-4">
-            <label tabIndex={0} className="" onClick={(e) => {
-              e.stopPropagation()
-            }}>
+            <label
+              tabIndex={0}
+              className=""
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
               <VerticalEllipsisIcon />
             </label>
             <ul
@@ -51,15 +65,21 @@ export default function PlanCard({ id, name, major }: PlanCardProps) {
               className="dropdown-content menu rounded-box w-36 bg-base-100 p-2 shadow"
             >
               <li>
-                <a href="#my-modal-2" className="" onClick={(e) => {
-                  e.stopPropagation()
-                }}>Delete Plan</a>
+                <a
+                  href="#my-modal-2"
+                  className=""
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                >
+                  Delete Plan
+                </a>
               </li>
             </ul>
           </div>
         </div>
         <p>{major}</p>
       </button>
-  </>
+    </>
   );
 }
