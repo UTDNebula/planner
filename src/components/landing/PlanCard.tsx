11d276bf111f1d53c27601a5cf619f0bd281a143
JCGarzaa <jgarza0127@yahoo.com>
2023-02-24 16:40:44 -0600

    resolve #365

--- src/components/landing/PlanCard.tsx
+++ src/components/landing/PlanCard.tsx
@@ -1,4 +1,7 @@
+import VerticalEllipsisIcon from '@/icons/VerticalEllipsisIcon';
+import { trpc } from '@/utils/trpc';
 import Tooltip from '@mui/material/Tooltip';
+import { utils } from 'mocha';
 import router from 'next/router';
 
 export type PlanCardProps = {
@@ -12,24 +15,51 @@ export default function PlanCard({ id, name, major }: PlanCardProps) {
     router.push(`/app/plans/${id}`);
   };
 
+  const utils = trpc.useContext()
+  const deletePlan = trpc.plan.deletePlanById.useMutation({
+    async onSuccess() {
+      await utils.plan.invalidate()
+    }
+  })
+
   return (
-    <Tooltip
-      title={name}
-      componentsProps={{
-        tooltip: {
-          className: 'p-5 text-[16px] bg-white text-primary-900 border-2  rounded-xl',
-        },
-      }}
-      followCursor
-      placement="top-start"
-    >
+    <>
+    <div className="modal" id="my-modal-2">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+        <p className="py-4">Are you sure you would like to delete this plan?</p>
+        <div className="modal-action">
+        <a href="#" className="btn alert-error" onClick={() => {deletePlan.mutateAsync(id)}}>Yes</a>
+        <a href="#" className="btn">No</a>
+        </div>
+      </div>
+    </div>
       <button
         onClick={handlePlanClick}
         className="flex h-[150px] w-full max-w-[300px] flex-col rounded-2xl bg-white py-6 px-8 text-left shadow-2xl transition-all hover:scale-110"
       >
-        <h4 className="max-w-full overflow-hidden text-ellipsis whitespace-nowrap">{name}</h4>
+        <div className='flex flex-row justify-between w-full'>
+          <h4 className="max-w-full overflow-hidden text-ellipsis whitespace-nowrap">{name}</h4>
+          <div className="dropdown pl-4">
+            <label tabIndex={0} className="" onClick={(e) => {
+              e.stopPropagation()
+            }}>
+              <VerticalEllipsisIcon />
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box w-36 bg-base-100 p-2 shadow"
+            >
+              <li>
+                <a href="#my-modal-2" className="" onClick={(e) => {
+                  e.stopPropagation()
+                }}>Delete Plan</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <p>{major}</p>
       </button>
-    </Tooltip>
+  </>
   );
 }
