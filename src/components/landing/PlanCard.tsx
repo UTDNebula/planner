e37377a746bd30ed639fa0f316086c9ad0fa7083
ZzRanger <calebmarklim@gmail.com>
2023-02-24 23:15:12 -0600

    update hamburger button styling on home page

--- src/components/landing/PlanCard.tsx
+++ src/components/landing/PlanCard.tsx
@@ -48,36 +48,39 @@ export default function PlanCard({ id, name, major }: PlanCardProps) {
         onClick={handlePlanClick}
         className="flex h-[150px] w-full max-w-[300px] flex-col rounded-2xl bg-white py-6 px-8 text-left shadow-2xl transition-all hover:scale-110"
       >
-        <div className="flex w-full flex-row justify-between">
-          <h4 className="max-w-full overflow-hidden text-ellipsis whitespace-nowrap">{name}</h4>
-          <div className="dropdown pl-4">
-            <label
-              tabIndex={0}
-              className=""
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              <VerticalEllipsisIcon />
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu rounded-box w-36 bg-base-100 p-2 shadow"
-            >
-              <li>
-                <a
-                  href="#my-modal-2"
-                  className=""
-                  onClick={(e) => {
-                    e.stopPropagation();
-                  }}
-                >
-                  Delete Plan
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className="flex w-full flex-row items-center justify-between">
+          <h4 className="overflow-hidden text-ellipsis whitespace-nowrap">{name}</h4>
+          <button
+            className="dropdown h-fit w-fit"
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+          >
+            <div className=" flex h-12 w-12 items-center justify-center rounded-full hover:bg-slate-100">
+              <label tabIndex={0}>
+                <VerticalEllipsisIcon />
+              </label>
+
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu rounded-box w-36 bg-base-100 p-2 shadow"
+              >
+                <li>
+                  <a
+                    href="#my-modal-2"
+                    className=""
+                    onClick={(e) => {
+                      e.stopPropagation();
+                    }}
+                  >
+                    Delete Plan
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </button>
         </div>
+
         <p>{major}</p>
       </button>
     </>
