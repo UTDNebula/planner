3fa0e7c08b136ab3de0a111b6d11932e97d5afa0
ZzRanger <calebmarklim@gmail.com>
2023-02-28 11:46:14 -0600

    add colors & hover effect

--- src/components/landing/PlanCard.tsx
+++ src/components/landing/PlanCard.tsx
@@ -16,45 +16,46 @@ export default function PlanCard({ id, name, major }: PlanCardProps) {
   return (
     <>
       {<PlanCardModal id={id} />}
-      <button
-        onClick={handlePlanClick}
-        className="flex h-[180px] w-full flex-col rounded-2xl bg-white py-6 px-8 text-left shadow-2xl transition-all hover:scale-110"
-      >
-        <div className="flex w-full flex-row items-center justify-between">
-          <h4 className="overflow-hidden text-ellipsis whitespace-nowrap text-[#A3A3A3]">{name}</h4>
-          <button
-            className="dropdown h-fit w-fit"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <div className=" flex h-12 w-12 items-center justify-center rounded-full hover:bg-slate-100">
-              <label tabIndex={0}>
-                <VerticalEllipsisIcon />
-              </label>
+      <div className="relative w-full rounded-2xl border-b-[12px] border-[#A3A3A3] border-b-primary bg-white text-[#1C2A6D] transition-all hover:scale-110 hover:border-b-[0px] hover:bg-primary hover:text-white">
+        <button
+          onClick={handlePlanClick}
+          className="flex h-[180px] w-full flex-col py-6 px-8 text-left"
+        >
+          <div className="flex w-full flex-row items-center justify-between">
+            <h4 className="overflow-hidden text-ellipsis whitespace-nowrap">{name}</h4>
+            <button
+              className="dropdown h-fit w-fit"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <div className=" flex h-12 w-12 items-center justify-center rounded-full hover:bg-slate-100">
+                <label tabIndex={0}>
+                  <VerticalEllipsisIcon className="" />
+                </label>
 
-              <ul
-                tabIndex={0}
-                className="dropdown-content menu rounded-box w-36 bg-base-100 p-2 shadow"
-              >
-                <li>
-                  <a
-                    href="#my-modal-2"
-                    className=""
-                    onClick={(e) => {
-                      e.stopPropagation();
-                    }}
-                  >
-                    Delete Plan
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </button>
-        </div>
-
-        <div className="flex flex-grow items-center text-xl font-semibold">{major}</div>
-      </button>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu rounded-box w-36 bg-base-100 p-2 shadow"
+                >
+                  <li>
+                    <a
+                      href="#my-modal-2"
+                      className=""
+                      onClick={(e) => {
+                        e.stopPropagation();
+                      }}
+                    >
+                      Delete Plan
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </button>
+          </div>
+          <div className="flex flex-grow items-center text-xl font-semibold">{major}</div>
+        </button>
+      </div>
     </>
   );
 }
