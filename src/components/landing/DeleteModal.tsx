b2332bf67b9ce16a85c8fdbd1a49c7e4b9f976da
Kevin G <ge.kevin.a@gmail.com>
2023-02-28 10:09:22 -0600

    Change delete description

--- src/components/landing/DeleteModal.tsx
+++ src/components/landing/DeleteModal.tsx
@@ -18,7 +18,7 @@ const DeleteModal: FC<DeleteModalProps> = ({ deletePlan, deleteLoading, ...props
       <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-generic-white p-6">
         <Dialog.Title className="text-lg font-medium">Delete plan</Dialog.Title>
         <Dialog.Description className="mt-2 text-base text-neutral-400">
-          {"Make changes to your profile here. Click save when you're done."}
+          {'Are you sure you want to delete? This action cannot be undone.'}
         </Dialog.Description>
         <div className="mt-4 flex gap-x-4">
           <Button
