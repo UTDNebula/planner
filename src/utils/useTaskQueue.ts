b1c27313b05cdc32d49a174f9573e90166416899
Kevin G <ge.kevin.a@gmail.com>
2023-03-20 16:01:10 -0500

    Change from useState to useRef for task queue

--- src/utils/useTaskQueue.ts
+++ src/utils/useTaskQueue.ts
@@ -1,54 +1,39 @@
 import React from 'react';
-import { trpc } from './trpc';
+
+export type Task<T> = {
+  func: (args: T) => Promise<unknown>;
+  args: T;
+};
+
+export type TaskQueueState = {
+  isProcessing: boolean;
+  tasks: Array<Task<unknown>>;
+};
 
 export function useTaskQueue(params: { shouldProcess: boolean }): {
   tasks: ReadonlyArray<Task<unknown>>;
   isProcessing: boolean;
   addTask: <T>(task: Task<T>) => void;
 } {
-  const utils = trpc.useContext();
-  const [queue, setQueue] = React.useState<{
-    isProcessing: boolean;
-    tasks: Array<Task<unknown>>;
-  }>({ isProcessing: false, tasks: [] });
+  const queue = React.useRef<TaskQueueState>({ isProcessing: false, tasks: [] });
 
   React.useEffect(() => {
-    if (!params.shouldProcess) return;
-    if (queue.tasks.length === 0) {
-      return;
-    }
-    if (queue.isProcessing) return;
+    if (!params.shouldProcess || queue.current.tasks.length === 0) return;
+    if (queue.current.isProcessing) return;
 
-    console.log(queue);
+    const { func, args } = queue.current.tasks[0];
 
-    const { func, args } = queue.tasks[0];
-    console.log(args);
-    setQueue((prev) => ({
-      isProcessing: true,
-      tasks: prev.tasks.slice(1),
-    }));
+    queue.current.isProcessing = true;
 
     Promise.resolve(func(args)).finally(() => {
-      setQueue((prev) => ({
-        isProcessing: false,
-        tasks: prev.tasks,
-      }));
+      queue.current.isProcessing = false;
+      queue.current.tasks.splice(0, 1);
     });
-  }, [queue, params.shouldProcess]);
+  }, [params.shouldProcess, queue.current.tasks.length]);
 
   return {
-    tasks: queue.tasks,
-    isProcessing: queue.isProcessing,
-    addTask: React.useCallback((task) => {
-      setQueue((prev) => ({
-        isProcessing: prev.isProcessing,
-        tasks: [...prev.tasks, task] as Task<unknown>[],
-      }));
-    }, []),
+    tasks: queue.current.tasks,
+    isProcessing: queue.current.isProcessing,
+    addTask: (task) => queue.current.tasks.push(task as Task<unknown>),
   };
 }
-
-export type Task<T> = {
-  func: (args: T) => Promise<unknown>;
-  args: T;
-};
