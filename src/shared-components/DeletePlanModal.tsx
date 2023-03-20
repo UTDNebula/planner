9add7465bc854085b6b42f5ad4c646555278064f
Kevin G <ge.kevin.a@gmail.com>
2023-03-20 12:41:28 -0500

    Use portal for modal

--- src/shared-components/DeletePlanModal.tsx
+++ src/shared-components/DeletePlanModal.tsx
@@ -1,5 +1,6 @@
 import Modal, { ModalProps } from '@/components/Modal';
 import { FC } from 'react';
+import { createPortal } from 'react-dom';
 import Button from '../components/Button';
 
 export interface DeleteModalProps extends ModalProps {
@@ -7,39 +8,36 @@ export interface DeleteModalProps extends ModalProps {
   deleteLoading: boolean;
 }
 
-const DeletePlanModal: FC<DeleteModalProps> = ({
-  deletePlan,
-  deleteLoading,
-  onClose,
-  ...props
-}) => (
-  <Modal onClose={onClose} {...props}>
-    <h1 className="text-lg font-medium">Delete plan</h1>
-    <p className="mt-2 text-base text-neutral-400">
-      {'Are you sure you want to delete? This action cannot be undone.'}
-    </p>
-    <div className="mt-4 flex gap-x-4">
-      <Button
-        className="ml-autoc"
-        color="tertiary"
-        onClick={(e) => {
-          onClose();
-          e.stopPropagation();
-        }}
-      >
-        Cancel
-      </Button>
-      <Button
-        isLoading={deleteLoading}
-        onClick={(e) => {
-          e.stopPropagation();
-          deletePlan();
-        }}
-      >
-        Delete
-      </Button>
-    </div>
-  </Modal>
-);
+const DeletePlanModal: FC<DeleteModalProps> = ({ deletePlan, deleteLoading, onClose, ...props }) =>
+  createPortal(
+    <Modal onClose={onClose} {...props}>
+      <h1 className="text-lg font-medium">Delete plan</h1>
+      <p className="mt-2 text-base text-neutral-400">
+        {'Are you sure you want to delete? This action cannot be undone.'}
+      </p>
+      <div className="mt-4 flex gap-x-4">
+        <Button
+          className="ml-autoc"
+          color="tertiary"
+          onClick={(e) => {
+            onClose();
+            e.stopPropagation();
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          isLoading={deleteLoading}
+          onClick={(e) => {
+            e.stopPropagation();
+            deletePlan();
+          }}
+        >
+          Delete
+        </Button>
+      </div>
+    </Modal>,
+    document.body,
+  );
 
 export default DeletePlanModal;
