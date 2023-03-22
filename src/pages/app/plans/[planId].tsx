5c254cbc2a149220cb8ba64b72b898f6e42e0646
ZzRanger <calebmarklim@gmail.com>
2023-03-21 21:04:45 -0500

    remove prefetch

--- src/pages/app/plans/[planId].tsx
+++ src/pages/app/plans/[planId].tsx
@@ -59,12 +59,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext<{ pl
   const planId = context.params?.planId as string;
 
   // await ssg.courses.publicGetSanitizedCourses.prefetch();
-  await Promise.all([
-    ssg.validator.prereqValidator.prefetch(planId),
-    ssg.validator.degreeValidator.prefetch(planId),
-    ssg.plan.getPlanById.prefetch(planId),
-    ssg.plan.getDegreeRequirements.prefetch({ planId }),
-  ]);
+  // await Promise.all([
+  //   ssg.validator.prereqValidator.prefetch(planId),
+  //   ssg.validator.degreeValidator.prefetch(planId),
+  //   ssg.plan.getPlanById.prefetch(planId),
+  //   ssg.plan.getDegreeRequirements.prefetch({ planId }),
+  // ]);
 
   return {
     props: {
