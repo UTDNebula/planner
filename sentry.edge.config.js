11d19577db6d3086d415dc25a834e2a422a85c82
Jason Antwi-Appah <hey@jasonaa.me>
2023-03-30 01:13:03 -0500

    style: fmt

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -7,7 +7,9 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://edb9e327f9024e7599eae859bad9be47@o4504918397353984.ingest.sentry.io/4504924302409728',
+  dsn:
+    SENTRY_DSN ||
+    'https://edb9e327f9024e7599eae859bad9be47@o4504918397353984.ingest.sentry.io/4504924302409728',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 0.25,
   // ...
