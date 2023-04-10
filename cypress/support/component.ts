00bc9a8ab57f52b5129784647f7f01b818f36215
Kevin Ge <ge.kevin.a@gmail.com>
2023-04-10 00:44:49 -0500

    Fix type issue

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -14,7 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import { ArgumentTypes } from '@/types/util-types';
 import './commands';
 
 // Alternatively you can use CommonJS syntax:
