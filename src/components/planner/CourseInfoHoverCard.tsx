6a198b9ed62f43aaa2846973feb5f0ebddf21e61
Kevin Ge <ge.kevin.a@gmail.com>
2023-04-14 07:28:21 -0500

    Rebase + fix hover card behaviors

--- src/components/planner/CourseInfoHoverCard.tsx
+++ src/components/planner/CourseInfoHoverCard.tsx
@@ -1,8 +1,9 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import * as HoverCard from '@radix-ui/react-hover-card';
 
 interface CourseInfoHoverCardProps {
-  prereqs: string[][];
+  // prereqs: string[][];
+  description: string;
   open: boolean;
   onOpenChange: (open: boolean) => void;
   side?: 'top' | 'left' | 'bottom' | 'right';
@@ -10,7 +11,8 @@ interface CourseInfoHoverCardProps {
 }
 
 export const CourseInfoHoverCard: FC<CourseInfoHoverCardProps> = ({
-  prereqs,
+  // prereqs,
+  description,
   open,
   onOpenChange,
   side = 'bottom',
@@ -21,48 +23,14 @@ export const CourseInfoHoverCard: FC<CourseInfoHoverCardProps> = ({
     <HoverCard.Trigger asChild>{children}</HoverCard.Trigger>
     <HoverCard.Portal>
       <HoverCard.Content
+        onClick={(e) => e.stopPropagation()}
         side={side}
-        className="z-[999] w-[250px] animate-[slideUpAndFade_0.3s] rounded-md border border-neutral-200 bg-generic-white p-5 shadow-sm"
+        className="z-[999] w-[350px] animate-[slideUpAndFade_0.3s] rounded-md border border-neutral-200 bg-generic-white p-5 shadow-sm"
         sideOffset={5}
       >
         <h3 className="mb-2 text-lg font-semibold">{title}</h3>
+        <CourseDescription description={description} />
 
-        {prereqs[0].length > 0 && (
-          <>
-            <h3 className="mb-2 text-lg font-medium">Prerequisites</h3>
-            <ol className="flex list-disc flex-wrap gap-3">
-              {prereqs[0].map((prereq) => (
-                <li key={prereq} className="ml-4">
-                  {prereq}
-                </li>
-              ))}
-            </ol>
-          </>
-        )}
-        {prereqs[1].length > 0 && (
-          <>
-            <h3 className="mb-2 text-lg font-medium">Corequisites</h3>
-            <ol className="flex list-disc flex-wrap gap-3">
-              {prereqs[1].map((prereq) => (
-                <li key={prereq} className="ml-4">
-                  {prereq}
-                </li>
-              ))}
-            </ol>
-          </>
-        )}
-        {prereqs[2].length > 0 && (
-          <>
-            <h3 className="mb-2 text-lg font-medium">Prerequisites or Corequisites</h3>
-            <ol className="flex list-disc flex-wrap gap-3">
-              {prereqs[2].map((prereq) => (
-                <li key={prereq} className="ml-4">
-                  {prereq}
-                </li>
-              ))}
-            </ol>
-          </>
-        )}
         <HoverCard.Arrow className="fill-primary" />
       </HoverCard.Content>
     </HoverCard.Portal>
@@ -70,3 +38,27 @@ export const CourseInfoHoverCard: FC<CourseInfoHoverCardProps> = ({
 );
 
 export default CourseInfoHoverCard;
+
+/**
+ * This component shows a course description.
+ * It also adds "Show more" or "Show less" button
+ * since course descriptions tend to be very long
+ */
+const CourseDescription = ({ description }: { description: string }) => {
+  const [showMore, setShowMore] = useState(false);
+  return (
+    <span>
+      <p className="text-sm">
+        {!showMore ? `${description.substring(0, 200)}...` : description}
+        <button
+          className="inline pl-2 font-medium text-primary"
+          onClick={(e) => {
+            setShowMore(!showMore);
+          }}
+        >
+          {showMore ? 'Show Less' : 'Show More'}
+        </button>
+      </p>
+    </span>
+  );
+};
--- src/components/planner/PrereqWarnHoverCard.tsx
+++ src/components/planner/PrereqWarnHoverCard.tsx
@@ -3,8 +3,9 @@ import * as HoverCard from '@radix-ui/react-hover-card';
 
 interface PrereqHoverCardProps {
   prereqs: [Array<string>, Array<string>, Array<string>];
+  description: string;
   open: boolean;
-  onOpenChange: (open: boolean) => void;
+  onOpenChange?: (open: boolean) => void;
   isOverriden: boolean;
   title: string;
   side?: 'top' | 'left' | 'bottom' | 'right';
@@ -24,7 +25,7 @@ export const PrereqWarnHoverCard: FC<PrereqHoverCardProps> = ({
     <HoverCard.Portal>
       <HoverCard.Content
         side={side}
-        className="z-[9999] w-[250px] animate-[slideUpAndFade_0.3s] rounded-md border border-neutral-200 bg-generic-white p-5 shadow-sm"
+        className="z-[9999] h-fit w-[250px] animate-[slideUpAndFade_0.3s] rounded-md border border-neutral-200 bg-generic-white p-5 shadow-sm"
         sideOffset={5}
       >
         <h3 className="mb-2 text-lg font-semibold">{title}</h3>
@@ -73,6 +74,44 @@ export const PrereqWarnHoverCard: FC<PrereqHoverCardProps> = ({
                 </ol>
               </>
             )}
+          </>
+        )}
+        {prereqs[0].length > 0 && (
+          <>
+            <h3 className="py-2 text-base">Prerequisites not met: </h3>
+            <ol className="flex list-disc flex-wrap gap-3">
+              {prereqs[0].map((prereq) => (
+                <li key={prereq} className="ml-4">
+                  {prereq}
+                </li>
+              ))}
+            </ol>
+          </>
+        )}
+        {prereqs[1].length > 0 && (
+          <>
+            <h3 className="py-2 text-base font-semibold">Corequisites not met: </h3>
+            <ol className="flex list-disc flex-wrap gap-3">
+              {prereqs[1].map((prereq) => (
+                <li key={prereq} className="ml-4">
+                  {prereq}
+                </li>
+              ))}
+            </ol>
+          </>
+        )}
+        {prereqs[2].length > 0 && (
+          <>
+            <h3 className="py-2 text-base font-semibold">
+              Corequisites or Prerequisites not met:{' '}
+            </h3>
+            <ol className="flex list-disc flex-wrap gap-3">
+              {prereqs[2].map((prereq) => (
+                <li key={prereq} className="ml-4">
+                  {prereq}
+                </li>
+              ))}
+            </ol>
           </>
         )}
         <HoverCard.Arrow className="fill-primary" />
