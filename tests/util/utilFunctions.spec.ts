17bb9983dcda28dc5a7392fbd7cabbc03f36c155
Kevin Ge <ge.kevin.a@gmail.com>
2023-07-10 19:49:35 -0700

    Add test for createSemesterCodeRange

--- tests/util/utilFunctions.spec.ts
+++ tests/util/utilFunctions.spec.ts
@@ -1,11 +1,14 @@
 import { describe, expect, test } from '@jest/globals';
 import {
+  createNewSemesterCode,
+  createSemesterCodeRange,
   createYearBasedOnFall,
   generateSemesters,
   getSemesterHourFromCourseCode,
 } from '../../src/utils/utilFunctions';
 import { Semester } from '@/components/planner/types';
 import { SemesterType } from '@prisma/client';
+import { SemesterCode } from 'prisma/utils';
 
 describe('createYearBasedOnFall', () => {
   test('0 year', () => {
@@ -131,6 +134,132 @@ describe('generateSemesters', () => {
 
   test.each(testCases)('$desc', (tc) => {
     const got = generateSemesters(tc.count, tc.startYear, tc.startSemester, tc.includeSummer);
+    expect(got).toMatchObject(tc.want);
+  });
+});
+
+describe('createNewSemesterCode', () => {
+  const testCases: { desc: string; input: SemesterCode; want: SemesterCode }[] = [
+    {
+      desc: 'Create semester following Fall 2000',
+      input: {
+        semester: 'f',
+        year: 2000,
+      },
+      want: {
+        semester: 's',
+        year: 2001,
+      },
+    },
+    {
+      desc: 'Create semester following Spring 2000',
+      input: {
+        semester: 's',
+        year: 2000,
+      },
+      want: {
+        semester: 'u',
+        year: 2000,
+      },
+    },
+    {
+      desc: 'Create semester following Summer 2000',
+      input: {
+        semester: 'u',
+        year: 2000,
+      },
+      want: {
+        semester: 'f',
+        year: 2000,
+      },
+    },
+  ];
+
+  test.each(testCases)('$desc', (tc) => {
+    const got = createNewSemesterCode(tc.input);
+    expect(got).toMatchObject(tc.want);
+  });
+});
+
+describe('createSemesterCodeRange', () => {
+  const testCases: {
+    desc: string;
+    startSemester: SemesterCode;
+    endSemester: SemesterCode;
+    includeEnd: boolean;
+    includeStart: boolean;
+    want: SemesterCode[];
+  }[] = [
+    {
+      desc: 'Create semester from Fall 2000 to Summer 2002 (inclusive on both ends)',
+      startSemester: {
+        semester: 'f',
+        year: 2000,
+      },
+      endSemester: {
+        semester: 'u',
+        year: 2002,
+      },
+      includeEnd: true,
+      includeStart: true,
+      want: [
+        { semester: 'f', year: 2000 },
+        { semester: 's', year: 2001 },
+        { semester: 'u', year: 2001 },
+        { semester: 'f', year: 2001 },
+        { semester: 's', year: 2002 },
+        { semester: 'u', year: 2002 },
+      ],
+    },
+    {
+      desc: 'Create semester from Fall 2000 to Summer 2002 (inclusive on start but not end)',
+      startSemester: {
+        semester: 'f',
+        year: 2000,
+      },
+      endSemester: {
+        semester: 'u',
+        year: 2002,
+      },
+      includeEnd: false,
+      includeStart: true,
+      want: [
+        { semester: 'f', year: 2000 },
+        { semester: 's', year: 2001 },
+        { semester: 'u', year: 2001 },
+        { semester: 'f', year: 2001 },
+        { semester: 's', year: 2002 },
+      ],
+    },
+    {
+      desc: 'Create semester from Fall 2000 to Summer 2002 (inclusive on end but not start)',
+      startSemester: {
+        semester: 'f',
+        year: 2000,
+      },
+      endSemester: {
+        semester: 'u',
+        year: 2002,
+      },
+      includeEnd: true,
+      includeStart: false,
+      want: [
+        { semester: 's', year: 2001 },
+        { semester: 'u', year: 2001 },
+        { semester: 'f', year: 2001 },
+        { semester: 's', year: 2002 },
+        { semester: 'u', year: 2002 },
+      ],
+    },
+  ];
+
+  test.each(testCases)('$desc', (tc) => {
+    const got = createSemesterCodeRange(
+      tc.startSemester,
+      tc.endSemester,
+      tc.includeEnd,
+      tc.includeStart,
+    );
     expect(got).toMatchObject(tc.want);
   });
 });
