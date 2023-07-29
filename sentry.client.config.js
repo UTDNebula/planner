b53cc8b780b7ebd8b810b467c26e171e2933cdac
Jason Antwi-Appah <hey@jasonaa.me>
2023-07-29 18:34:26 -0500

    Increase Sentry sample rate

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -11,7 +11,7 @@ Sentry.init({
     SENTRY_DSN ||
     'https://edb9e327f9024e7599eae859bad9be47@o4504918397353984.ingest.sentry.io/4504924302409728',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.25,
+  tracesSampleRate: 0.75,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
@@ -19,7 +19,7 @@ Sentry.init({
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
+  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.3 : 1,
   // If the entire session is not sampled, use the below sample rate to sample
   // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
