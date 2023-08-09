793df8a5caee21dc041fd53ff93978114cc729e4
aravk <aravkasi@gmail.com>
2023-08-09 16:00:53 -0500

    Remove unused import and rename var

--- validator/degree_solver.py
+++ validator/degree_solver.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from enum import Enum
-from copy import deepcopy
 from pydantic import Json
 
 from typing import Any
@@ -175,15 +174,15 @@ class DegreeRequirementsSolver:
                 [course for course in self.courses], []
             )  # Convert to list
         # Set of the core courses that are fulfilled, so they won't be considered as free electives
-        core_courses = set()
+        used_core_courses = set()
         for req_fill in self.solved_core.reqs_to_courses.values():
             for course in req_fill.keys():
-                core_courses.add(course.name)
+                used_core_courses.add(course.name)
         # Run for major
         for degree_req in self.degree_requirements:
             for course in self.courses:
                 # Make sure it's not a core course
-                if course in core_courses:
+                if course in used_core_courses:
                     continue
                 for requirement in degree_req.requirements:
                     if requirement.attempt_fulfill(course):
