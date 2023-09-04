9a7c387571d2e8d74460fc373a1c79a0a916ce5e
aravk <aravkasi@gmail.com>
2023-09-03 23:50:33 -0500

    Redo major files

---
+++ validator/scripts/match_degree_names.py
@@ -0,0 +1,32 @@
+import json
+import re
+import os
+import uuid
+from glob import glob
+from pathlib import Path
+
+re = re.compile(r"(\w) \(")
+
+degree_files = set()
+
+for fname in glob("validator/degree_data/*.json"):
+    with open(fname, "r") as f:
+        data = json.load(f)
+        if "abbreviation" in data:
+            data["display_name"] = re.sub(r"\1(", data["abbreviation"])
+            del data["abbreviation"]
+        if "uuid" not in data:
+            data["uuid"] = str(uuid.uuid4())
+    # Delete the old file
+    os.remove(fname)
+    # Write to the new file using the new naming convention
+    new_name = re.sub(r"\1(", fname)
+    with open(new_name, "w") as f:
+        json.dump(data, f, indent=2)
+        degree_files.add(Path(new_name).stem)
+
+with open("src/data/majors.json", "r") as f:
+    data = json.load(f)
+    degree_names = set(data)
+
+print("\n".join(degree_files.difference(degree_names)))
