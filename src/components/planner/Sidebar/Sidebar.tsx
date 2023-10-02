3537ef1ce5d169e276c7d5b07e4cd749e0d50b7d
Saidarsh <saidarshtukkadi@gmail.com>
2023-10-02 04:35:08 -0500

    drag and drop deletion feature

--- src/components/planner/Sidebar/Sidebar.tsx
+++ src/components/planner/Sidebar/Sidebar.tsx
@@ -1,13 +1,8 @@
+import { UniqueIdentifier, useDroppable } from '@dnd-kit/core';
 import * as Dialog from '@radix-ui/react-dialog';
 import { useRef, useState, useMemo, memo } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import { v4 as uuidv4 } from 'uuid';
-
-import AccordionSkeleton from './AccordionSkeleton';
-import DraggableCourseList from './DraggableCourseList';
-import { DegreeRequirement } from './types';
-import { Course, DraggableCourse, GetDragIdByCourse } from '../types';
-import useFuse from '../useFuse';
 
 import Button from '@/components/Button';
 import AnalyticsWrapper from '@/components/common/AnalyticsWrapper';
@@ -17,6 +12,12 @@ import ChevronIcon from '@/icons/ChevronIcon';
 import { trpc } from '@/utils/trpc';
 import { getSemesterHourFromCourseCode } from '@/utils/utilFunctions';
 
+import AccordionSkeleton from './AccordionSkeleton';
+import DraggableCourseList from './DraggableCourseList';
+import { DegreeRequirement } from './types';
+import { Course, DragDataToSidebarTile, DraggableCourse, GetDragIdByCourse } from '../types';
+import useFuse from '../useFuse';
+
 import 'react-loading-skeleton/dist/skeleton.css';
 
 export interface CourseSelectorContainerProps {
@@ -25,6 +26,8 @@ export interface CourseSelectorContainerProps {
   transferCredits: string[];
   getSearchedDragId: GetDragIdByCourse;
   getRequirementDragId: GetDragIdByCourse;
+  dropId: UniqueIdentifier;
+  dragActive?: boolean;
 }
 
 function CourseSelectorContainer({
@@ -33,6 +36,8 @@ function CourseSelectorContainer({
   transferCredits,
   getSearchedDragId,
   getRequirementDragId,
+  dropId,
+  dragActive,
 }: CourseSelectorContainerProps) {
   const {
     data: validationData,
@@ -49,6 +54,13 @@ function CourseSelectorContainer({
         skipBatch: true,
       },
     },
+  });
+
+  const isDroppable = dragActive;
+
+  const { setNodeRef } = useDroppable({
+    id: dropId,
+    data: { to: 'sidebar-tile' } as DragDataToSidebarTile,
   });
 
   const { data, isLoading } = trpc.courses.publicGetAllCourses.useQuery();
@@ -117,115 +129,141 @@ function CourseSelectorContainer({
   return (
     <>
       {open ? (
-        <div
-          id="tutorial-editor-1"
-          className="z-0 h-screen w-[30%] min-w-[30%] overflow-x-hidden overflow-y-scroll"
-        >
-          <div className="flex h-fit min-h-screen w-full flex-col gap-y-4 bg-white p-4">
-            <div className="flex flex-col">
-              <div className="flex flex-col justify-between xl:flex-row xl:items-center">
-                <div className="flex flex-row items-center xl:justify-center">
-                  <ChevronIcon
-                    onClick={() => setOpen(!open)}
-                    className={`h-4 w-4 cursor-pointer ${open ? '' : 'rotate-180'}`}
-                    strokeWidth={2.5}
-                  />
-                  <h1 className="whitespace-nowrap pl-2 text-2xl font-medium tracking-tight">
-                    Plan Requirements
-                  </h1>
+        open && dragActive ? (
+          <div
+            ref={isDroppable ? setNodeRef : ref}
+            id="tutorial-editor-1"
+            className="z-0 h-screen w-[30%] min-w-[30%] overflow-x-hidden overflow-y-scroll"
+          >
+            <div className="flex h-fit min-h-screen w-full flex-col gap-y-4 bg-white p-4">
+              <div className="h-[96vh] rounded-lg border-2 border-dashed border-red-400">
+                <div className="flex h-screen items-center justify-center">
+                  Drop here to delete course
                 </div>
-                <div id="tutorial-editor-2">
-                  {validationData ? (
-                    <CreditsTaken
-                      taken={sum}
-                      min={
-                        validationData.validation.requirements.length > 0
-                          ? validationData.validation.requirements[1].min_hours
-                          : 120
-                      }
-                    />
-                  ) : (
-                    <CreditsTaken taken={-1} min={-1} />
-                  )}
-                </div>
-              </div>
-              <h6 className="text-base tracking-tight text-gray-500">
-                Drag courses onto your plan
-              </h6>
-            </div>
-            <div className="z-[999] drop-shadow-2xl">
-              <AnalyticsWrapper analyticsClass="umami--click--search-course">
-                <SearchBar
-                  onClick={() => setDisplay(true)}
-                  updateQuery={(q) => {
-                    updateQuery(q);
-                    setDisplay(true);
-                  }}
-                  className={`${
-                    displayResults
-                      ? 'rounded-b-none border-b-transparent'
-                      : 'rounded-b-[10px] border-b-inherit'
-                  }`}
-                  placeholder="Search courses"
-                />
-              </AnalyticsWrapper>
-              <div className="relative">
-                <div
-                  ref={ref}
-                  className="absolute z-[99] w-full overflow-clip rounded-b-[10px] bg-white"
-                ></div>
-              </div>
-            </div>
-
-            <Dialog.Root open={displayResults} onOpenChange={(v) => setDisplay(v)} modal={false}>
-              {ref.current && (
-                <Dialog.Portal className="z-[99]" container={ref?.current}>
-                  <Dialog.Content
-                    asChild
-                    className="z-[999]"
-                    onOpenAutoFocus={(e) => e.preventDefault()}
-                  >
-                    <div className="w-full border-[2px] border-[#EDEFF7] bg-white p-4 drop-shadow-2xl">
-                      <DraggableCourseList courses={courseResults} getDragId={getSearchedDragId} />
-                    </div>
-                  </Dialog.Content>
-                </Dialog.Portal>
-              )}
-            </Dialog.Root>
-
-            {error?.data?.code === 'INTERNAL_SERVER_ERROR' && (
-              <div className="flex h-[30vh] w-full text-base leading-5 text-[#A3A3A3]">
-                <div className="mx-12 mt-44 flex w-full flex-col items-center justify-center gap-4 text-center leading-6">
-                  It seems like a screw has gone loose!
-                  <a href="https://airtable.com/shrFg9MPi9BGguwPU">
-                    <Button>REPORT ERROR</Button>
-                  </a>
-                </div>
-              </div>
-            )}
-
-            {validationData &&
-              validationData.validation.requirements.length > 0 &&
-              validationData.validation.requirements.map((req: DegreeRequirement, idx: number) => (
-                <RequirementsContainer
-                  key={idx}
-                  degreeRequirement={req}
-                  courses={courses}
-                  getCourseItemDragId={getRequirementDragId}
-                />
-              ))}
-            {!validationData && <AccordionSkeleton />}
-            <div className="flex flex-grow items-end justify-end text-sm ">
-              <div>
-                <span className="font-bold">Warning:</span> This is an unofficial tool not
-                affiliated with the university. For official advice, please consult the course
-                catalog and confirm with your department advisors. <br /> <br />
-                Problems with degree or prerequisite validation? Request edits here or send an email
-                to planner@utdnebula.com.
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div
+            id="tutorial-editor-1"
+            className="z-0 h-screen w-[30%] min-w-[30%] overflow-x-hidden overflow-y-scroll"
+          >
+            <div className="flex h-fit min-h-screen w-full flex-col gap-y-4 bg-white p-4">
+              <div className="flex flex-col">
+                <div className="flex flex-col justify-between xl:flex-row xl:items-center">
+                  <div className="flex flex-row items-center xl:justify-center">
+                    <ChevronIcon
+                      onClick={() => setOpen(!open)}
+                      className={`h-4 w-4 cursor-pointer ${open ? '' : 'rotate-180'}`}
+                      strokeWidth={2.5}
+                    />
+                    <h1 className="whitespace-nowrap pl-2 text-2xl font-medium tracking-tight">
+                      Plan Requirements
+                    </h1>
+                  </div>
+                  <div id="tutorial-editor-2">
+                    {validationData ? (
+                      <CreditsTaken
+                        taken={sum}
+                        min={
+                          validationData.validation.requirements.length > 0
+                            ? validationData.validation.requirements[1].min_hours
+                            : 120
+                        }
+                      />
+                    ) : (
+                      <CreditsTaken taken={-1} min={-1} />
+                    )}
+                  </div>
+                </div>
+                <h6 className="text-base tracking-tight text-gray-500">
+                  Drag courses onto your plan
+                </h6>
+              </div>
+              <div className="z-[999] drop-shadow-2xl">
+                <AnalyticsWrapper analyticsClass="umami--click--search-course">
+                  <SearchBar
+                    onClick={() => setDisplay(true)}
+                    updateQuery={(q) => {
+                      updateQuery(q);
+                      setDisplay(true);
+                    }}
+                    className={`${
+                      displayResults
+                        ? 'rounded-b-none border-b-transparent'
+                        : 'rounded-b-[10px] border-b-inherit'
+                    }`}
+                    placeholder="Search courses"
+                  />
+                </AnalyticsWrapper>
+                <div className="relative">
+                  <div
+                    ref={ref}
+                    className="absolute z-[99] w-full overflow-clip rounded-b-[10px] bg-white"
+                  ></div>
+                </div>
+              </div>
+
+              <Dialog.Root open={displayResults} onOpenChange={(v) => setDisplay(v)} modal={false}>
+                {ref.current && (
+                  <Dialog.Portal className="z-[99]" container={ref?.current}>
+                    <Dialog.Content
+                      asChild
+                      className="z-[999]"
+                      onOpenAutoFocus={(e) => e.preventDefault()}
+                    >
+                      <div className="w-full border-[2px] border-[#EDEFF7] bg-white p-4 drop-shadow-2xl">
+                        <DraggableCourseList
+                          courses={courseResults}
+                          getDragId={getSearchedDragId}
+                        />
+                      </div>
+                    </Dialog.Content>
+                  </Dialog.Portal>
+                )}
+              </Dialog.Root>
+
+              {error?.data?.code === 'INTERNAL_SERVER_ERROR' && (
+                <div className="flex h-[30vh] w-full text-base leading-5 text-[#A3A3A3]">
+                  <div className="mx-12 mt-44 flex w-full flex-col items-center justify-center gap-4 text-center leading-6">
+                    It seems like a screw has gone loose!
+                    <a href="https://airtable.com/shrFg9MPi9BGguwPU">
+                      <Button>REPORT ERROR</Button>
+                    </a>
+                  </div>
+                </div>
+              )}
+
+              {validationData &&
+                validationData.validation.requirements.length > 0 &&
+                validationData.validation.requirements.map(
+                  (req: DegreeRequirement, idx: number) => (
+                    <RequirementsContainer
+                      key={idx}
+                      degreeRequirement={req}
+                      courses={courses}
+                      getCourseItemDragId={getRequirementDragId}
+                    />
+                  ),
+                )}
+              {!validationData && <AccordionSkeleton />}
+              <div className="flex flex-grow items-end justify-end text-sm ">
+                <div>
+                  <span className="font-bold">Warning:</span> This is an unofficial tool not
+                  affiliated with the university. For official advice, please consult the course
+                  catalog and confirm with your department advisors. <br /> <br />
+                  Problems with degree or prerequisite validation? Request edits here or send an
+                  email to planner@utdnebula.com.
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      ) : dragActive ? (
+        <div
+          ref={isDroppable ? setNodeRef : ref}
+          className="z-0 flex h-screen w-[50px] flex-col items-center border border-neutral-300 bg-red-500 py-8"
+        ></div>
       ) : (
         <div className="z-0 flex h-screen w-[50px] flex-col items-center border border-neutral-300 bg-white py-8">
           <ChevronIcon
