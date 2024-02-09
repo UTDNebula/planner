19bbba9386b62b2ddedb9d5446f27e5085fe9dd5
ishaanthenerd <145402353+ishaanthenerd@users.noreply.github.com>
2024-02-09 23:46:48 +0000

    Deleted CustomPlan.tsx changes

--- src/components/template/CustomPlan.tsx
+++ src/components/template/CustomPlan.tsx
@@ -129,10 +129,6 @@ export default function CustomPlan({ onDismiss }: { onDismiss: () => void }) {
       }
     }
 
-    console.log(keywords); // *DELETE*
-    const classIndexes = []; // *DELETE*
-    const earnedIndexes = []; // *DELETE*
-
     // TODO: Consider whether credit was earned or not before adding to credits list
     const credits: Credit[] = [];
     let isTransfer = true;
@@ -140,7 +136,6 @@ export default function CustomPlan({ onDismiss }: { onDismiss: () => void }) {
     for (let j = 0; j < keywords.length; j++) {
       const code = keywords[j];
       if (courseCode.includes(code) && j < keywords.length - 1) {
-        classIndexes.push(j); // *DELETE*
         let attemptedIndex = j;
         while (
           isNaN(parseFloat(keywords[attemptedIndex])) ||
@@ -149,11 +144,9 @@ export default function CustomPlan({ onDismiss }: { onDismiss: () => void }) {
           attemptedIndex++;
         }
         const earnedIndex = attemptedIndex + 1;
-        earnedIndexes.push(earnedIndex); // *DELETE*
-        const creditEarned = true; // *FIX*
 
         const digit = keywords[j + 1].slice(0, 4).replace(/^\s+|\s+$/g, '');
-        if (/^[\d-]+$/.test(digit) && creditEarned) {
+        if (/^[\d-]+$/.test(digit)) {
           credits.push({
             semesterCode: term,
             courseCode: `${code} ${digit}`,
@@ -175,9 +168,6 @@ export default function CustomPlan({ onDismiss }: { onDismiss: () => void }) {
         }
       }
     }
-
-    console.log(classIndexes); // *DELETE*
-    console.log(earnedIndexes); // *DELETE*
 
     const dedupedCredits = credits.reduce((acc, curr) => {
       if (!acc.some((i) => i.courseCode === curr.courseCode)) {
